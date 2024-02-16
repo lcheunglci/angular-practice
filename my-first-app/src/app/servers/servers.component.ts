8087c54c58d26ff6faa71515cfad1695c1d6875d
Lawrence LCI <lcheunglci@gmail.com>
2024-02-15 23:37:59 -0800

    Add examples of selectors by attr and by class name

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
+  //selector: '[app-servers]', // select by attribute name
+  //selector: '.app-servers', // select by class
   template: `<app-server></app-server>
     <app-server></app-server> `,
   styleUrl: './servers.component.css',
