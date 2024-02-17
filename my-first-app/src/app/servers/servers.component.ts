68b6c2239e481f4809f7cd376d335168d7298c4a
Lawrence LCI <lcheunglci@gmail.com>
2024-02-16 23:32:10 -0800

    Add property binding

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -4,8 +4,22 @@ import { Component } from '@angular/core';
   selector: 'app-servers',
   //selector: '[app-servers]', // select by attribute name
   //selector: '.app-servers', // select by class
-  template: `<app-server></app-server>
-    <app-server></app-server> `,
+  //template: `<app-server></app-server>
+  //  <app-server></app-server> `,
+  templateUrl: './servers.component.html',
   styleUrl: './servers.component.css',
 })
-export class ServersComponent {}
+export class ServersComponent {
+  allowNewServer = false;
+
+  /**
+   *
+   */
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
+
+  ngOnInit() {}
+}
