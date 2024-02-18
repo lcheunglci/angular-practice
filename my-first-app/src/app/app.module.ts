1ce9731e59fbdaeb9932f012b626cf9eac4aedce
Lawrence LCI <lcheunglci@gmail.com>
2024-02-17 23:58:24 -0800

    Add a app component exercise for databinding

--- my-first-app/src/app/app.module.ts
+++ my-first-app/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
 import { SuccessAlertComponent } from './success-alert/success-alert.component';
 import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
@@ -14,7 +15,7 @@ import { WarningAlertComponent } from './warning-alert/warning-alert.component';
     SuccessAlertComponent,
     WarningAlertComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
