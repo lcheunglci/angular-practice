e2b8581069906d9b3310e5772339aace902692cc
Lawrence LCI <lcheunglci@gmail.com>
2024-02-27 23:43:21 -0800

    Use ViewChild ElementRef to switch from 2 way binding for the serverContent input

--- databinding-practice/src/app/cockpit/cockpit.component.ts
+++ databinding-practice/src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,10 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  Output,
+  ViewChild,
+} from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
@@ -19,7 +25,9 @@ export class CockpitComponent {
   }>();
 
   // newServerName = '';
-  newServerContent = '';
+  // newServerContent = '';
+  @ViewChild('serverContentInput', { static: true })
+  serverContentInput: ElementRef;
 
   //onAddServer() {
   //  this.serverCreated.emit({
@@ -33,14 +41,14 @@ export class CockpitComponent {
 
     this.serverCreated.emit({
       serverName: nameInput.value,
-      serverContent: this.newServerContent,
+      serverContent: this.serverContentInput.nativeElement.value,
     });
   }
 
   onAddBlueprint(nameInput: HTMLInputElement) {
     this.blueprintCreated.emit({
       serverName: nameInput.value,
-      serverContent: this.newServerContent,
+      serverContent: this.serverContentInput.nativeElement.value,
     });
   }
 }
