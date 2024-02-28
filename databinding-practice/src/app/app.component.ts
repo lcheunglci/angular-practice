5cf206472b6452e9f3e58e2015ce78432eb6cabe
Lawrence LCI <lcheunglci@gmail.com>
2024-02-27 23:29:37 -0800

    Explicitly show the mode of encapsulation of style

--- databinding-practice/src/app/app.component.ts
+++ databinding-practice/src/app/app.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
+  encapsulation: ViewEncapsulation.Emulated, // the default value, but can use None, and Native
 })
 export class AppComponent {
   serverElements = [
