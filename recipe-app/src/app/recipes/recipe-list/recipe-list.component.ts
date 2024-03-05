e5d943958d4085ef989a4a9b29fcca65ad9e8385
Lawrence LCI <lcheunglci@gmail.com>
2024-03-04 22:55:33 -0800

    Add property and event binding to the recipe component

--- recipe-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipe-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,10 +7,16 @@ import { Recipe } from '../recipe.model';
   styleUrl: './recipe-list.component.css',
 })
 export class RecipeListComponent {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe(
       'A Test Recipe',
       'This is simply a test',
+      'https://unsplash.com/photos/uQs1802D0CQ/download?ixid=M3wxMjA3fDB8MXxhbGx8fHx8fHx8fHwxNzA4NzU5NDUzfA&force=true&w=640'
+    ),
+    new Recipe(
+      'Another Test Recipe',
+      'This is simply another test',
       'https://unsplash.com/photos/uQs1802D0CQ/download?ixid=M3wxMjA3fDB8MXxhbGx8fHx8fHx8fHwxNzA4NzU5NDUzfA&force=true&w=640'
     ),
   ];
@@ -18,4 +24,8 @@ export class RecipeListComponent {
   constructor() {}
 
   ngOnInit() {}
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
 }
