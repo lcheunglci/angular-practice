c4248be3c589ff65225f047c2ae6d43385010f8c
Lawrence LCI <lcheunglci@gmail.com>
2024-03-05 22:02:15 -0800

    Add the start game button

--- databinding-assignment/src/app/game-control/game-control.component.ts
+++ databinding-assignment/src/app/game-control/game-control.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Output } from '@angular/core';
+import { EventEmitter } from 'stream';
 
 @Component({
   selector: 'app-game-control',
   templateUrl: './game-control.component.html',
-  styleUrl: './game-control.component.css'
+  styleUrl: './game-control.component.css',
 })
-export class GameControlComponent {
+export class GameControlComponent implements OnInit {
+  @Output() intervalFired = new EventEmitter<number>();
+  interval;
+  lastNumber = 0;
+  constructor() {}
 
+  ngOnInit(): void {}
+
+  onStartGame() {
+    this.interval = setInterval(() => {
+      this.intervalFired.emit(this.lastNumber + 1);
+      this.lastNumber++;
+    }, 1000);
+  }
 }
