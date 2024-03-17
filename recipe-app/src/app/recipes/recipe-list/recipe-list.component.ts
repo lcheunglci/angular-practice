803e0d74955dfe82acf161ef69479ed96ccbf5c2
Lawrence LCI <lcheunglci@gmail.com>
2024-03-16 20:21:19 -0700

    Use the recipe service for cross component communication

--- recipe-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipe-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component } from '@angular/core';
 
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
@@ -9,7 +9,6 @@ import { RecipeService } from '../recipe.service';
   styleUrl: './recipe-list.component.css',
 })
 export class RecipeListComponent {
-  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[];
 
   constructor() { }
@@ -18,7 +17,4 @@ export class RecipeListComponent {
     this.recipes = recipeService.getRecipes();
   }
 
-  onRecipeSelected(recipe: Recipe) {
-    this.recipeWasSelected.emit(recipe);
-  }
 }
