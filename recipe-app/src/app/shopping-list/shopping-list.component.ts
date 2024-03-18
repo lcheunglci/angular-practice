b256bedd428e2d0e80526c18cf6c72dfb842a074
Lawrence LCI <lcheunglci@gmail.com>
2024-03-17 22:52:55 -0700

    Use service to push data changes using event emitter

--- recipe-app/src/app/shopping-list/shopping-list.component.ts
+++ recipe-app/src/app/shopping-list/shopping-list.component.ts
@@ -12,6 +12,9 @@ export class ShoppingListComponent implements OnInit {
 
   ngOnInit() {
     this.ingredients = this.slService.getIngredients();
+    this.slService.ingredientsChanged.subscribe((ingredients: Ingredient[]) => {
+      this.ingredients = ingredients;
+    });
   }
 
 }
