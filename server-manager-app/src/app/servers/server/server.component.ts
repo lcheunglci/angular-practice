657b6186a4977e6829fecbc3d80d4cd04fce3142
Lawrence LCI <lcheunglci@gmail.com>
2024-03-27 23:43:46 -0700

    Preserve the query params when navigating

--- server-manager-app/src/app/servers/server/server.component.ts
+++ server-manager-app/src/app/servers/server/server.component.ts
@@ -27,7 +27,8 @@ export class ServerComponent implements OnInit {
 
   onEdit() {
     this.router.navigate(['edit'], {
-      relativeTo: this.route
+      relativeTo: this.route,
+      queryParamsHandling: 'preserve'
     });
   }
 
