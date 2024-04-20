03da91d0554906b1f9a68c50f31fb720be1d7567
Lawrence LCI <lcheunglci@gmail.com>
2024-04-19 23:40:59 -0700

    Add the ReactiveForm in modules

--- forms-reactive-app/src/app/app.component.ts
+++ forms-reactive-app/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { FormGroup } from '@angular/forms'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   genders = ['male', 'female'];
+  signupForm: FormGroup;
 }
