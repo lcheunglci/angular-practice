ba0de53ae4b0fb3d70ec143f7e4cea8e79330da1
Lawrence LCI <lcheunglci@gmail.com>
2024-04-23 23:59:44 -0700

    Add reset after submit and setValue

--- forms-reactive-app/src/app/app.component.ts
+++ forms-reactive-app/src/app/app.component.ts
@@ -27,7 +27,19 @@ export class AppComponent implements OnInit {
     this.signupForm.statusChanges.subscribe(
       (status) => console.log(status)
     );
-
+    this.signupForm.setValue({
+      'userData': {
+        'username': 'Max',
+        'email': 'max@test.com'
+      },
+      'gender': 'male',
+      'hobbies': []
+    });
+    this.signupForm.patch({
+      'userData': {
+        'username': 'Sam',
+      },
+    });
 
   }
 
