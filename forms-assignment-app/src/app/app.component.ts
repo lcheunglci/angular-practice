b52811c50f07db7ff82c619c7c96b6b9bd80c5f8
Lawrence LCI <lcheunglci@gmail.com>
2024-04-25 23:20:18 -0700

    Add form validators

--- forms-assignment-app/src/app/app.component.ts
+++ forms-assignment-app/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { CustomValidators } from './custom-validators';
 
 @Component({
   selector: 'app-root',
@@ -11,7 +12,8 @@ export class AppComponent implements OnInit {
 
   ngOnInit(): void {
     this.projectForm = new FormGroup({
-      'projectName': new FormControl(null, Validators.required),
+      'projectName': new FormControl(null,
+        [Validators.required, CustomValidators.invalidProjectName, CustomValidators.asyncInvalidProjectName]),
       'email': new FormControl(null, [Validators.required, Validators.email]),
       'projectStatus': new FormControl('critical'),
     })
