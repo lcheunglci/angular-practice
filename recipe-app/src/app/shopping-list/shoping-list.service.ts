bb36ee43a483d6c5ef52bcd516e54e5a60f9a114
Lawrence LCI <lcheunglci@gmail.com>
2024-04-29 23:39:08 -0700

    Add delete functionality

--- recipe-app/src/app/shopping-list/shoping-list.service.ts
+++ recipe-app/src/app/shopping-list/shoping-list.service.ts
@@ -30,4 +30,9 @@ export class ShoppingListService {
     this.ingredients[index] = newIngredient;
     this.ingredientsChanged.next(this.ingredients.slice());
   }
+
+  deleteIngredient(index: number) {
+    this.ingredients = this.ingredients.splice(index, 1);
+    this.ingredientsChanged.next(this.ingredients.slice());
+  }
 }
