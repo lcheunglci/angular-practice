81b1e73f2a254fe709335b0200d3c340be942c1f
Lawrence LCI <lcheunglci@gmail.com>
2024-05-09 00:17:41 -0700

    Add post when submit

--- http-app/src/app/app.component.ts
+++ http-app/src/app/app.component.ts
@@ -9,13 +9,20 @@ import { HttpClient } from '@angular/common/http';
 export class AppComponent implements OnInit {
   loadedPosts = [];
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
-  ngOnInit() {}
+  ngOnInit() { }
 
   onCreatePost(postData: { title: string; content: string }) {
     // Send Http request
-    console.log(postData);
+    // console.log(postData);
+    //
+    this.http.post(
+      process.env['URL'],
+      postData).subscribe(responseData => {
+        console.log(responseData);
+      });
+
   }
 
   onFetchPosts() {
