b2123abac73476f8d67fcd31880c17e248efa114
Lawrence LCI <lcheunglci@gmail.com>
2024-05-16 00:04:45 -0700

    Add put request when save data button is pressed

--- recipe-app/src/app/shared/data-storage.service.ts
+++ recipe-app/src/app/shared/data-storage.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
+import { RecipeService } from "../recipes/recipe.service";
 
 @Injectable({ providedIn: 'root' })
 export class DataStorageService {
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient, private recipeService: RecipeService) {
 
+  }
+
+  storeRecipes() {
+    const recipe = this.recipeService.getRecipes();
+    this.http.put(process.env['URL'], recipes).subscribe(response =>
+      console.log(response)
+    );
   }
 }
