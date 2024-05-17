c67909b2444688e7efe7259320234ea2c2763e5f
Lawrence LCI <lcheunglci@gmail.com>
2024-05-17 00:02:40 -0700

    Resolve Data before loading using a recipe resolver service

--- recipe-app/src/app/shared/data-storage.service.ts
+++ recipe-app/src/app/shared/data-storage.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { RecipeService } from "../recipes/recipe.service";
 import { Recipe } from "../recipes/recipe.model";
-import { map } from "rxjs";
+import { map, tap } from "rxjs";
 
 @Injectable({ providedIn: 'root' })
 export class DataStorageService {
@@ -18,13 +18,18 @@ export class DataStorageService {
   }
 
   fetchRecipes() {
-    this.http.get<Recipe[]>(process.env['URL'])
+    return this.http.get<Recipe[]>(process.env['URL'])
       .pipe(map(recipes => {
-        return recipes.map(recipe => { return { ...recipe, ingredients: recipe.ingredients ? recipe.ingredients : [] } })
-      }))
-      .subscribe(recipes => {
-        //console.log(recipes);
-        this.recipeService.setRecipes(recipes);
-      })
+        return recipes.map(
+          recipe => {
+            return {
+              ...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []
+            }
+          }),
+          tap(recipes => {
+            this.recipeService.setRecipes(recipes);
+          })
+      }));
+
   }
 }
