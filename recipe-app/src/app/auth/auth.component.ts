7fa9be424e0a9811f791c368aae48da13fc06bf8
Lawrence LCI <lcheunglci@gmail.com>
2024-05-22 23:37:12 -0700

    Update the UI to show the logged in state

--- recipe-app/src/app/auth/auth.component.ts
+++ recipe-app/src/app/auth/auth.component.ts
@@ -2,17 +2,18 @@ import { Component } from "@angular/core";
 import { NgForm } from "@angular/forms";
 import { AuthResponseData, AuthService } from "./auth.service";
 import { Observable } from "rxjs";
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html'
 })
 export class AuthComponent {
-  isLoginMode: true;
-  isLoading: false;
+  isLoginMode = true;
+  isLoading = false;
   error: string = null;
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private router: Router) { }
 
   onSwitchMode() {
     this.isLoginMode = !this.isLoginMode;
@@ -33,12 +34,13 @@ export class AuthComponent {
     if (this.isLoginMode) {
       authObs = this.authService.login(email, password);
     } else {
-      authObs.this.authService.signup(email, password);
+      authObs = this.authService.signup(email, password);
     }
     authObs.subscribe(
       resData => {
         console.log(resData);
         this.isLoading = false;
+        this.router.navigate(['/recipes']);
       }, errorMessage => {
         console.log(errorMessage);
         this.error = errorMessage;
