c0e94aa060402fd9a1610bd7493769c1f66528e5
Lawrence LCI <lcheunglci@gmail.com>
2024-05-23 23:45:21 -0700

    Add token to the request

--- recipe-app/src/app/shared/data-storage.service.ts
+++ recipe-app/src/app/shared/data-storage.service.ts
@@ -1,35 +1,48 @@
 import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpParams } from "@angular/common/http";
 import { RecipeService } from "../recipes/recipe.service";
 import { Recipe } from "../recipes/recipe.model";
-import { map, tap } from "rxjs";
+import { exhaustMap, map, take, tap } from "rxjs";
+import { AuthService } from "../auth/auth.service";
 
 @Injectable({ providedIn: 'root' })
 export class DataStorageService {
-  constructor(private http: HttpClient, private recipeService: RecipeService) {
-
-  }
+  constructor(
+    private http: HttpClient,
+    private recipeService: RecipeService,
+    private authService: AuthService
+  ) { }
 
   storeRecipes() {
-    const recipe = this.recipeService.getRecipes();
-    this.http.put(process.env['URL'], recipes).subscribe(response =>
+    const recipes = this.recipeService.getRecipes();
+    const url: string = process.env['URL'];
+    this.http.put(url, recipes).subscribe(response =>
       console.log(response)
     );
   }
 
   fetchRecipes() {
-    return this.http.get<Recipe[]>(process.env['URL'])
-      .pipe(map(recipes => {
+    return this.authService.user.pipe(
+      take(1),
+      exhaustMap(user => {
+        const url: string = process.env['URL'];
+        return this.http.get<Recipe[]>(url, {
+          params: new HttpParams().set('auth', user.token)
+        });
+      }),
+      map(recipes => {
         return recipes.map(
           recipe => {
             return {
-              ...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []
+              ...recipe,
+              ingredients: recipe.ingredients ? recipe.ingredients : []
             }
-          }),
-          tap(recipes => {
-            this.recipeService.setRecipes(recipes);
-          })
-      }));
+          });
+      }),
+      tap(recipes => {
+        this.recipeService.setRecipes(recipes);
+      })
+    )
 
   }
 }
