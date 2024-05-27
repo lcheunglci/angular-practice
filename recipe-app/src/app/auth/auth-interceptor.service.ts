d7c9d2ba258a13451fd2aa87c191fff351c8bd54
Lawrence LCI <lcheunglci@gmail.com>
2024-05-26 23:53:55 -0700

    Fix the auth interceptor service

--- recipe-app/src/app/auth/auth-interceptor.service.ts
+++ recipe-app/src/app/auth/auth-interceptor.service.ts
@@ -1,12 +1,12 @@
-import { HttpInterceptor, HttpParams } from "@angular/common/http";
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpParams, HttpRequest } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { AuthService } from "./auth.service";
-import { exhaustMap, take } from "rxjs";
+import { Observable, exhaustMap, take } from "rxjs";
 
 @Injectable()
 export class AuthInterceptorService implements HttpInterceptor {
 
-  constructor(private authService: AuthService)
+  constructor(private authService: AuthService) { }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return this.authService.user.pipe(
@@ -21,6 +21,5 @@ export class AuthInterceptorService implements HttpInterceptor {
         return next.handle(modifiedReq);
 
       }))
-    return next.handle(req);
   }
 }
