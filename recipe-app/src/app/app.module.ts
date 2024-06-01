ba91c9e372a54afc4deca653ff41ec6879e40d3c
Lawrence LCI <lcheunglci@gmail.com>
2024-05-31 23:39:12 -0700

    Add the shared module

--- recipe-app/src/app/app.module.ts
+++ recipe-app/src/app/app.module.ts
@@ -4,28 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { DropdownDirective } from './shared/dropdown.directive';
 import { ShoppingListService } from './shopping-list/shoping-list.service';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RecipeService } from './recipes/recipe.service';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { AuthComponent } from './auth/auth.component';
-import { LoadingSpinnerComponent } from './shared/loading-spinner/loading-spinner.component';
 import { AuthInterceptorService } from './auth/auth-interceptor.service';
-import { AlertComponent } from './shared/alert/alert.component';
-import { PlaceHolderDirective } from './shared/placeholder/placeholder.directive';
 import { RecipesModule } from './recipes/recipes.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    DropdownDirective,
     AuthComponent,
-    LoadingSpinnerComponent,
-    AlertComponent,
-    PlaceHolderDirective
   ],
   imports: [
     BrowserModule,
@@ -34,13 +27,10 @@ import { ShoppingListModule } from './shopping-list/shopping-list.module';
     HttpClientModule,
     AppRoutingModule,
     RecipesModule,
-    ShoppingListModule
+    ShoppingListModule,
+    SharedModule
   ],
   providers: [ShoppingListService, RecipeService, { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptorService, multi: true }],
   bootstrap: [AppComponent]
-  //,
-  //entryComponents: [
-  //  AlertComponent
-  //]
 })
 export class AppModule { }
--- recipe-app/src/app/shopping-list/shopping-list.module.ts
+++ recipe-app/src/app/shopping-list/shopping-list.module.ts
@@ -2,7 +2,8 @@ import { NgModule } from "@angular/core";
 import { ShoppingEditComponent } from "./shopping-edit/shopping-edit.component";
 import { ShoppingListComponent } from "./shopping-list.component";
 import { RouterModule } from "@angular/router";
-import { CommonModule } from "@angular/common";
+import { FormsModule } from "@angular/forms";
+import { SharedModule } from "../shared/shared.module";
 
 @NgModule({
   declarations: [
@@ -10,10 +11,11 @@ import { CommonModule } from "@angular/common";
     ShoppingEditComponent,
   ],
   imports: [
-    CommonModule,
+    FormsModule,
     RouterModule.forChild([
       { path: 'shopping-list', component: ShoppingListComponent },
-    ])
+    ]),
+    SharedModule
   ]
 })
 export class ShoppingListModule {
