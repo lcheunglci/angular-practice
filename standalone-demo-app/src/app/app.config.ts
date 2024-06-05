84e53ddda40602d2eb2a2771910354103f9944be
Lawrence LCI <lcheunglci@gmail.com>
2024-06-04 23:33:04 -0700

    Add routing to the new components

--- standalone-demo-app/src/app/app.config.ts
+++ standalone-demo-app/src/app/app.config.ts
@@ -1,10 +1,11 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { AppRoutingModule } from './app-routing.module';
 // import { AnalyticsService } from './shared/analytics.service';
 
 export const appConfig: ApplicationConfig = {
   // providers: [AnalyticsService, provideRouter(routes)]
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), importProvidersFrom(AppRoutingModule)]
 };
