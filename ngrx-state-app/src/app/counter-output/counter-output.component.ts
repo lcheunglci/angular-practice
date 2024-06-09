ae8f63b0a4b872bcb82bcd2f98588669a970e137
Lawrence LCI <lcheunglci@gmail.com>
2024-06-08 23:51:28 -0700

    Add selectors for the count

--- ngrx-state-app/src/app/counter-output/counter-output.component.ts
+++ ngrx-state-app/src/app/counter-output/counter-output.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Observable } from 'rxjs';
 
 import { Store } from '@ngrx/store';
+import { selectCount, selectDoubleCount } from '../store/counter.selectors';
 
 @Component({
   selector: 'app-counter-output',
@@ -11,9 +12,11 @@ import { Store } from '@ngrx/store';
 export class CounterOutputComponent {
 
   count$: Observable<number>;
+  countDouble$: Observable<number>;
 
   constructor(private store: Store<{ counter: number }>) {
-    this.count$ = store.select('counter');
+    this.count$ = store.select(selectCount);
+    this.countDouble$ = store.select(selectDoubleCount);
   }
 
 
