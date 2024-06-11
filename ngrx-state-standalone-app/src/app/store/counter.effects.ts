cb18cc33371832d470ec4387508d6054efd2f71e
Lawrence LCI <lcheunglci@gmail.com>
2024-06-10 23:21:14 -0700

    Use the store data in effects

--- ngrx-state-standalone-app/src/app/store/counter.effects.ts
+++ ngrx-state-standalone-app/src/app/store/counter.effects.ts
@@ -1,17 +1,22 @@
 import { Actions, createEffect, ofType } from "@ngrx/effects";
-import { tap } from "rxjs";
+import { tap, withLatestFrom } from "rxjs";
 import { Injectable } from "@angular/core";
 import { decrement, increment } from "./counter.actions";
+import { Store } from "@ngrx/store";
+import { selectCount } from "./counter.selectors";
 
 @Injectable()
 export class CounterEffects {
-  saveCount = createEffect(() => this.actions$.pipe(
-    ofType(increment, decrement),
-    tap((action) => {
-      console.log(action);
-      localStorage.setItem('count', action.value.toString());
-    })
-  ), { dispatch: false });
+  saveCount = createEffect(
+    () =>
+      this.actions$.pipe(
+        ofType(increment, decrement),
+        withLatestFrom(this.store.select(selectCount)),
+        tap(([action, counter]) => {
+          console.log(action);
+          localStorage.setItem('count', counter.toString());
+        })
+      ), { dispatch: false });
 
-  constructor(private actions$: Actions) { }
+  constructor(private actions$: Actions, private store: Store<{ counter: number }>) { }
 }
