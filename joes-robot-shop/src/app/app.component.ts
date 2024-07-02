f021c3aeaffd213ef58ae94945b1a440b3604af3
Lawrence LCI <lcheunglci@gmail.com>
2024-07-01 18:36:22 -0700

    Add catalog and site-header component

--- joes-robot-shop/src/app/app.component.ts
+++ joes-robot-shop/src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from "./home/home.component";
+import { CatalogComponent } from './catalog/catalog.component';
+import { SiteHeaderComponent } from './site-header/site-header.component';
 
 @Component({
     selector: 'bot-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, HomeComponent]
+    imports: [RouterOutlet, HomeComponent, CatalogComponent, SiteHeaderComponent]
 })
 export class AppComponent {
   title = 'joes-robot-shop';
