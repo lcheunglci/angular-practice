afe667984e3899e98d2c5480960a583cc6d38c91
Lawrence LCI <lcheunglci@gmail.com>
2024-07-05 22:25:24 -0700

    Add event emitter with output to send event back to parent

--- joes-robot-shop/src/app/product-details/product-details.component.ts
+++ joes-robot-shop/src/app/product-details/product-details.component.ts
@@ -1,23 +1,25 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { IProduct } from '../catalog/product.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'bot-product-details',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './product-details.component.html',
   styleUrl: './product-details.component.css'
 })
 export class ProductDetailsComponent {
 
   @Input() product!: IProduct;
+  @Output() buy = new EventEmitter();
 
   getImageUrl(product: IProduct) {
     return '/assets/images/robot-parts/' + product.imageName;
   }
 
-  addToCart(product: IProduct) {
-
+  buyButtonClicked(product: IProduct) {
+    this.buy.emit();
   }
 
   getDiscountedClasses(product: IProduct) {
