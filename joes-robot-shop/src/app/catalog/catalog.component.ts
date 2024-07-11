4a6a6c8240c3511629a4abb99c1eaf2bedcfbbc1
Lawrence LCI <lcheunglci@gmail.com>
2024-07-11 07:49:55 -0700

    Add navigate to cart when the buy button is clicked

--- joes-robot-shop/src/app/catalog/catalog.component.ts
+++ joes-robot-shop/src/app/catalog/catalog.component.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { ProductDetailsComponent } from '../product-details/product-details.component';
 import { CartService } from '../cart/cart.service';
 import { ProductService } from './product.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'bot-catalog',
@@ -16,18 +17,21 @@ export class CatalogComponent {
   products: any;
   filter: string = '';
 
-  constructor(private cartSvc: CartService, private productSvc: ProductService) {
-    
-  }
+  constructor(
+    private cartSvc: CartService,
+    private productSvc: ProductService,
+    private router: Router
+  ) {}
 
   ngOnInit() {
-    this.productSvc.getProducts().subscribe(products => {
+    this.productSvc.getProducts().subscribe((products) => {
       this.products = products;
     });
   }
 
   addToCart(product: IProduct) {
     this.cartSvc.add(product);
+    this.router.navigate(['/cart']);
   }
 
   getFilteredProducts() {
