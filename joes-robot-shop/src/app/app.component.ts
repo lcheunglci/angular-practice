86e3d0f7540a832a621cc0d8f2c45ab6bc24dc16
Lawrence LCI <lcheunglci@gmail.com>
2024-07-15 22:41:57 -0700

    Add 2-way data binding to the credentials

--- joes-robot-shop/src/app/app.component.ts
+++ joes-robot-shop/src/app/app.component.ts
@@ -4,6 +4,7 @@ import { HomeComponent } from "./home/home.component";
 import { CatalogComponent } from './catalog/catalog.component';
 import { SiteHeaderComponent } from './site-header/site-header.component';
 import { CartComponent } from './cart/cart.component';
+import { SignInComponent } from './user/sign-in/sign-in.component';
 
 
 @Component({
@@ -11,7 +12,7 @@ import { CartComponent } from './cart/cart.component';
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, HomeComponent, CatalogComponent, SiteHeaderComponent, CartComponent]
+    imports: [RouterOutlet, HomeComponent, CatalogComponent, SiteHeaderComponent, CartComponent, SignInComponent]
 })
 export class AppComponent {
   title = 'joes-robot-shop';
--- joes-robot-shop/src/app/user/sign-in/sign-in.component.ts
+++ joes-robot-shop/src/app/user/sign-in/sign-in.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { IUserCredentials } from '../user.model';
 
 @Component({
   selector: 'bot-sign-in',
+  standalone: true,
   templateUrl: './sign-in.component.html',
   styleUrls: ['./sign-in.component.css'],
+  imports: [FormsModule]
 })
+
 export class SignInComponent {
 
+  credentials: IUserCredentials = {email: '', password: ''}
   constructor() { }
 
 }
