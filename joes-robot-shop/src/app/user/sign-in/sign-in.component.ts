8230f1e6561c077d77e50aa940c7607767aaccd1
Lawrence LCI <lcheunglci@gmail.com>
2024-07-15 22:47:37 -0700

    Add submit handler to the sign-in

--- joes-robot-shop/src/app/user/sign-in/sign-in.component.ts
+++ joes-robot-shop/src/app/user/sign-in/sign-in.component.ts
@@ -1,18 +1,27 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { IUserCredentials } from '../user.model';
+import { UserService } from '../user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'bot-sign-in',
   standalone: true,
   templateUrl: './sign-in.component.html',
   styleUrls: ['./sign-in.component.css'],
-  imports: [FormsModule]
+  imports: [FormsModule],
 })
-
 export class SignInComponent {
 
-  credentials: IUserCredentials = {email: '', password: ''}
-  constructor() { }
+  credentials: IUserCredentials = { email: '', password: '' };
+  constructor(private userService: UserService, private router: Router ) {}
+
+  signIn() {
+    this.userService.signIn(this.credentials).subscribe(
+      {
+        next: ()=> this.router.navigate(['/catalog']);
+      }
+    );
+  }
 
 }
