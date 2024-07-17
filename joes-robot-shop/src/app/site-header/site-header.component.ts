cd48344fbac3897d5937006ac7b44772d6f5f360
Lawrence LCI <lcheunglci@gmail.com>
2024-07-16 21:27:14 -0700

    Show error when login failed and show profile icon in header bar when signed in

--- joes-robot-shop/src/app/site-header/site-header.component.ts
+++ joes-robot-shop/src/app/site-header/site-header.component.ts
@@ -1,13 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IUser } from '../user/user.model';
+import { UserService } from '../user/user.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'bot-site-header',
   standalone: true,
   templateUrl: './site-header.component.html',
-  styleUrls: ['./site-header.component.css']
+  styleUrls: ['./site-header.component.css'],
+  imports: [CommonModule]
 })
-export class SiteHeaderComponent {
+export class SiteHeaderComponent implements OnInit {
+  user: IUser | null = null;
+  showSignOutMenu: boolean = false;
 
-  constructor() { }
+  constructor(private userService: UserService) {}
 
+  ngOnInit(): void {
+    this.userService.getUser().subscribe({
+      next: (user) => {
+        this.user = user;
+      },
+    });
+  }
+
+  toggleSignOutMenu() {
+    this.showSignOutMenu = !this.showSignOutMenu;
+  }
+
+  signOut() {
+    this.userService.signOut();
+    this.showSignOutMenu = false;
+  }
 }
--- joes-robot-shop/src/app/user/sign-in/sign-in.component.ts
+++ joes-robot-shop/src/app/user/sign-in/sign-in.component.ts
@@ -3,23 +3,28 @@ import { FormsModule } from '@angular/forms';
 import { IUserCredentials } from '../user.model';
 import { UserService } from '../user.service';
 import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'bot-sign-in',
   standalone: true,
   templateUrl: './sign-in.component.html',
   styleUrls: ['./sign-in.component.css'],
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
 })
 export class SignInComponent {
 
   credentials: IUserCredentials = { email: '', password: '' };
+  signInError: boolean = false;
+
   constructor(private userService: UserService, private router: Router ) {}
 
   signIn() {
+    this.signInError = false;
     this.userService.signIn(this.credentials).subscribe(
       {
-        next: ()=> this.router.navigate(['/catalog']);
+        next: ()=> this.router.navigate(['/catalog']),
+        error: () => (this.signInError = true)
       }
     );
   }
