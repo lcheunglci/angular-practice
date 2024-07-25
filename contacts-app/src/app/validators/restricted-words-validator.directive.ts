4924d6fe7afa517eaa5558c5f73d6784ac273eac
Lawrence LCI <lcheunglci@gmail.com>
2024-07-25 00:06:39 -0700

    Add input to custom validator

--- contacts-app/src/app/validators/restricted-words-validator.directive.ts
+++ contacts-app/src/app/validators/restricted-words-validator.directive.ts
@@ -1,25 +1,37 @@
-import { Directive } from "@angular/core";
-import { AbstractControl, NG_VALIDATORS, ValidationErrors, Validator } from "@angular/forms";
+import { Directive, Input } from '@angular/core';
+import {
+  AbstractControl,
+  NG_VALIDATORS,
+  ValidationErrors,
+  Validator,
+} from '@angular/forms';
 
 @Directive({
   selector: '[restrictedWords]',
   standalone: true,
-  providers: [{
-    provide: NG_VALIDATORS,
-    multi: true,
-    useExisting: RestrictedWordsValidator
-  }]
+  providers: [
+    {
+      provide: NG_VALIDATORS,
+      multi: true,
+      useExisting: RestrictedWordsValidator,
+    },
+  ],
 })
 export class RestrictedWordsValidator implements Validator {
+  @Input('restrictedWords') restrictedWords: string[] = [];
+
   validate(control: AbstractControl): ValidationErrors | null {
     if (!control.value) {
       return null;
     }
-    
-    return control.value.includes('foo') ? {restrictedWords: true} : null;
+
+    const invalidWords = this.restrictedWords
+      .map((w) => (control.value.include(w) ? w : null))
+      .filter((w) => w !== null);
+
+    return invalidWords.length > 0 ? { restrictedWords: invalidWords.join(',') } : null;
   }
   registerOnValidatorChange?(fn: () => void): void {
-    throw new Error("Method not implemented.");
+    throw new Error('Method not implemented.');
   }
-
 }
