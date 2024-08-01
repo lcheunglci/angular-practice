e1007f61e104e7ced1e8f063e9399fcbfc4f089b
Lawrence LCI <lcheunglci@gmail.com>
2024-08-01 00:23:56 -0700

    Handle input events for new directive and implement the interface

--- contacts-app-reactive/src/app/date-value-accessor/date-value-accessor.directive.ts
+++ contacts-app-reactive/src/app/date-value-accessor/date-value-accessor.directive.ts
@@ -1,5 +1,11 @@
-import { Directive, forwardRef, Provider } from '@angular/core';
-import { NG_VALIDATORS } from '@angular/forms';
+import {
+  Directive,
+  ElementRef,
+  forwardRef,
+  HostListener,
+  Provider,
+} from '@angular/core';
+import { ControlValueAccessor, NG_VALIDATORS } from '@angular/forms';
 
 const DATE_VALUE_PROVIDER: Provider = {
   provide: NG_VALIDATORS,
@@ -12,6 +18,34 @@ const DATE_VALUE_PROVIDER: Provider = {
     'input([type=date])[formControlName],input([type=date])[formControl],input([type=date])[ngModel]',
   providers: [DATE_VALUE_PROVIDER],
 })
-export class DateValueAccessorDirective {
-  constructor() {}
+export class DateValueAccessorDirective implements ControlValueAccessor {
+  constructor(private element: ElementRef) {}
+
+  @HostListener('input', ['$event.target.valueAsDate'])
+  private onChange!: Function;
+
+  @HostListener('blur')
+  private onTouched!: Function;
+  
+
+  writeValue(newValue: any): void {
+    if (newValue instanceof Date) {
+      // yyyy-mm-dd
+      this.element.nativeElement.value = newValue.toISOString().split('T')[0];
+    }
+  }
+
+  registerOnChange(fn: Function): void {
+    this.onChange = (valueAsDate: Date) => {
+      fn(valueAsDate);
+    };
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+
+  setDisabledState?(isDisabled: boolean): void {
+    throw new Error('Method not implemented.');
+  }
 }
