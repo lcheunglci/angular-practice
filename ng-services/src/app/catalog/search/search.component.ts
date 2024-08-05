0cad5ab283d3dc58d96b8178158e159716914efe
Lawrence LCI <lcheunglci@gmail.com>
2024-08-04 23:38:55 -0700

    Add ProductsService - Update catalog and search component to use DI with the ProductsService

--- ng-services/src/app/catalog/search/search.component.ts
+++ ng-services/src/app/catalog/search/search.component.ts
@@ -1,21 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Product } from '../product.model';
-import { productsArray } from '../products-data'
+import { ProductService } from '@catalog/products.service';
 
 @Component({
   selector: 'bot-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.css'],
 })
-export class SearchComponent {
-  products: Product[] = [...productsArray];
+export class SearchComponent implements OnInit {
+  products: Product[] = [];
   searchTerm: string = '';
   cart: Product[] = [];
 
-  constructor() { }
+  constructor(private productsService: ProductService) {}
 
-  ngOnInit() {
-    this.products = [...productsArray];
+  ngOnInit(): void {
+    this.products = this.productsService.getProducts();
   }
 
   addToCart(product: Product) {
@@ -29,8 +29,8 @@ export class SearchComponent {
   getFilteredProducts() {
     return this.searchTerm === ''
       ? this.products
-      : this.products.filter(
-        (product: Product) => product.name.toLowerCase().includes(this.searchTerm)
-      );
+      : this.products.filter((product: Product) =>
+          product.name.toLowerCase().includes(this.searchTerm)
+        );
   }
 }
