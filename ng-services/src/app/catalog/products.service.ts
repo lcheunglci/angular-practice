16f7879c9d1ea33780ff5399f90d88233856f8b3
Lawrence LCI <31262254+lcheunglci@users.noreply.github.com>
2024-08-08 22:25:09 -0700

    Update products.service.ts
    making api call in products service

--- ng-services/src/app/catalog/products.service.ts
+++ ng-services/src/app/catalog/products.service.ts
@@ -1,17 +1,15 @@
 import { Product } from "@shared/product.model";
 import { Injectable } from "@angular/core";
 import { Observable, Subject } from "rxjs";
+import { HttpClient } from "@angular/common/http";
 
 @Injectable({ providedIn: 'root'})
 export class ProductService {
 
-  private products: Subject<Product[]> = new Subject();
+  constructor(private httpClient: HttpClient) {}
 
   getProducts(): Observable<Product[]> {
-
-    // fetch('/api/product')
-
-    return this.products;
+    return this.httpClient.get<Product[]>('/api/products');
   }
 
 }
