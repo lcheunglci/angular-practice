4e98949d9706f8920f7ed8b17820f7a18dc9ca4e
Lawrence LCI <lcheunglci@gmail.com>
2024-08-09 22:50:39 -0700

    Use the async pipe for the catalog component

--- ng-services/src/app/catalog/catalog.component.ts
+++ ng-services/src/app/catalog/catalog.component.ts
@@ -1,26 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Product } from './product.model';
 import { ProductService } from './products.service';
 import { CartService } from '@core/cart.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'bot-catalog',
   templateUrl: './catalog.component.html',
   styleUrls: ['./catalog.component.css'],
 })
-export class CatalogComponent implements OnInit {
-  products: Product[] = [];
+export class CatalogComponent {
+  products: Observable<Product[]> = this.productsService.getProducts();
 
   constructor(
     private productsService: ProductService,
     private cartService: CartService
   ) {}
 
-  ngOnInit(): void {
-    this.productsService.getProducts().subscribe(
-      (products) => this.products = products
-    );
-  }
 
   addToCart(product: Product) {
     this.cartService.add(product);
