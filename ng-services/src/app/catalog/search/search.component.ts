d2becd9f784a1903ec27e856ad774fc16b5bd956
Lawrence LCI <lcheunglci@gmail.com>
2024-08-10 20:44:43 -0700

    Add the inject and cart service token to the search and cart component

--- ng-services/src/app/catalog/search/search.component.ts
+++ ng-services/src/app/catalog/search/search.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { Product } from '../product.model';
 import { ProductService } from '@catalog/products.service';
-import { CartService } from '@core/cart.service';
+import { CART_SERVICE_TOKEN, CartService } from '@core/cart.service';
 
 @Component({
   selector: 'bot-search',
@@ -14,7 +14,7 @@ export class SearchComponent implements OnInit {
 
   constructor(
     private productsService: ProductService,
-    private cartService: CartService
+    @Inject(CART_SERVICE_TOKEN) private cartService: CartService
   ) {}
 
   ngOnInit(): void {
