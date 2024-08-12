09ad3b2b870566071b4543f846e547c68de42811
Lawrence LCI <lcheunglci@gmail.com>
2024-08-11 23:49:52 -0700

    Add localstorage to the cart service

--- ng-services/src/app/core/cart.service.ts
+++ ng-services/src/app/core/cart.service.ts
@@ -1,26 +1,59 @@
 import { computed, Injectable, InjectionToken, signal } from '@angular/core';
 import { Product } from '@shared/product.model';
 
+type CartOptions = {
+  persistenceType: string;
+  persistenceKey: string;
+};
+
 @Injectable({ providedIn: 'root' })
 export class CartService {
   private cartItems = signal<Product[]>([]);
+  private cartOptions: CartOptions = {
+    persistenceType: 'local',
+    persistenceKey: 'cart',
+  };
+
+  constructor() {
+    if (this.cartOptions && this.cartOptions.persistenceKey === 'local') {
+      const cartString = localStorage.getItem(this.cartOptions.persistenceKey);
+      const cart: Product[] = cartString
+        ? (JSON.parse(cartString) as Product[])
+        : [];
+      this.cartItems.set(cart);
+    }
+  }
 
   get cart() {
     return this.cartItems.asReadonly();
   }
 
   add(product: Product) {
-    this.cartItems.update( (oldCart) => [...oldCart, product]);
+    this.cartItems.update((oldCart) => [...oldCart, product]);
+    this.storeCart();
   }
 
   remove(product: Product) {
     this.cartItems.update((oldCart) => oldCart.filter((p) => p !== product));
+    this.storeCart();
+  }
+
+  private storeCart() {
+    if (this.cartOptions && this.cartOptions.persistenceKey === 'local') {
+      localStorage.setItem(
+        this.cartOptions.persistenceKey,
+        JSON.stringify(this.cartItems())
+      );
+    }
   }
 
   get cartTotal() {
-    return computed( () =>  this.cartItems().reduce((prev, next) => {
-      let discount = next.discount && next.discount > 0 ? 1 - next.discount : 1;
-      return prev + next.price * discount;
-    }, 0));
+    return computed(() =>
+      this.cartItems().reduce((prev, next) => {
+        let discount =
+          next.discount && next.discount > 0 ? 1 - next.discount : 1;
+        return prev + next.price * discount;
+      }, 0)
+    );
   }
 }
