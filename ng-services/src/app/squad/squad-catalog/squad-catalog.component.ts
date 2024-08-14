3264f9244775e80c55c1726142f18b5003e01602
Lawrence LCI <lcheunglci@gmail.com>
2024-08-13 22:41:17 -0700

    move the provider from module to squad catalog

--- ng-services/src/app/squad/squad-catalog/squad-catalog.component.ts
+++ ng-services/src/app/squad/squad-catalog/squad-catalog.component.ts
@@ -1,15 +1,20 @@
 import { Component, Inject } from '@angular/core';
 import { Product } from '@shared/product.model';
-import { engineers } from './engineers';
 import { CartService } from '@core/cart.service';
 import { IProductsService, IProductsServiceToken } from '@shared/products-service.interface';
 import { Observable } from 'rxjs';
+import { EngineersService } from '../engineers.service';
 
 @Component({
   selector: 'bot-catalog',
   templateUrl: './squad-catalog.component.html',
   styleUrls: ['./squad-catalog.component.css'],
-  providers: []
+  providers: [
+    {
+      provide: IProductsServiceToken,
+      useClass: EngineersService,
+    },
+  ]
 })
 export class SquadCatalogComponent {
   squad: Observable<Product[]> = this.engineersService.getProducts();
--- ng-services/src/app/squad/squad.module.ts
+++ ng-services/src/app/squad/squad.module.ts
@@ -26,10 +26,10 @@ import { EngineersService } from './engineers.service';
       },
       deps: [CART_OPTIONS_TOKEN],
     },
-    {
-      provide: IProductsServiceToken,
-      useClass: EngineersService,
-    },
+    // {
+    //   provide: IProductsServiceToken,
+    //   useClass: EngineersService,
+    // },
   ],
 })
 export class SquadModule {}
