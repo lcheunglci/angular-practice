55d0cd4ac56f0e89247ecdd8c122f6a1300bab65
Lawrence LCI <lcheunglci@gmail.com>
2024-08-28 23:02:52 -0700

    Add context to elements to translate

--- ng-i18n/src/app/hero/hero.component.ts
+++ ng-i18n/src/app/hero/hero.component.ts
@@ -8,11 +8,11 @@ import { Component } from '@angular/core';
         <div class="hero-image"><img src="assets/img/hero-1.png" alt="Image of homemade blueberry pie" i18n-alt/></div>
       </div>
       <div class="logo-container">
-        <img src="assets/img/logomark.png" alt="Logomark for Bethany's Pies" i18n-alt/>
+        <img src="assets/img/logomark.png" alt="Logomark for Bethany's Pies" i18n-alt="logo|Logo for business@@businessLogo"/>
       </div>
 
       <div class="focused-message">
-        <h2 i18n>Pies for every occasion</h2>
+        <h2 i18n="site description|A description about this site's product line@@siteDescription">Pies for every occasion</h2>
         <p i18n>Visit our cafe or ship to your home</p>
       </div>
     </section>
@@ -21,5 +21,5 @@ import { Component } from '@angular/core';
     standalone: true
 })
 export class HeroComponent {
-  componentText = $localize;
+  componentText = $localize `translate menu text for example`;
 }
