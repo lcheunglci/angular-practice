9258847149bae8d430896b15da4a5334cb354f55
Lawrence LCI <lcheunglci@gmail.com>
2024-08-30 23:02:44 -0700

    Add the click listener to button

--- ng-refresher/src/app/persons/person-input.component.ts
+++ ng-refresher/src/app/persons/person-input.component.ts
@@ -1,8 +1,12 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-person-input',
   templateUrl: './person-input.component.html',
-  styleUrl: './person-input.component.css'
+  styleUrl: './person-input.component.css',
 })
-export class PersonInputComponent {}
+export class PersonInputComponent {
+  onCreatePerson(personName: string) {
+    console.log('Created a person :' + personName);
+  }
+}
