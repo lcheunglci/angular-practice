8d92207ab63ea87b8a508bd3abc1ddf8433a0a36
Lawrence LCI <lcheunglci@gmail.com>
2024-08-31 22:58:45 -0700

    Use Output to append to the user list

--- ng-refresher/src/app/app.component.ts
+++ ng-refresher/src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   persons = ['Alice', 'Bob', 'Carl']
+
+
+  onPersonCreated(name: string) {
+    this.persons.push(name);
+  }
 }
--- ng-refresher/src/app/persons/person-input.component.ts
+++ ng-refresher/src/app/persons/person-input.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-person-input',
@@ -7,10 +7,12 @@ import { Component } from '@angular/core';
 })
 export class PersonInputComponent {
 
+  @Output() personCreate = new EventEmitter<string>();
   enteredPersonName = '';
 
   onCreatePerson() {
     console.log('Created a person :' + this.enteredPersonName);
     this.enteredPersonName = '';
+    this.personCreate.emit(this.enteredPersonName);
   }
 }
