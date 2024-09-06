a6597523c59576e0143f80e86f0e9fe40cd685ef
Lawrence LCI <lcheunglci@gmail.com>
2024-09-05 21:51:12 -0700

    Add ion alert controller to display the invalid input

--- ng-ionic-app/app.js
+++ ng-ionic-app/app.js
@@ -4,6 +4,7 @@ const cancelBtn = document.querySelector('#btn-cancel');
 const confirmBtn = document.querySelector('#btn-confirm');
 const expensesList = document.querySelector('#expenses-list');
 const totalExpensesOutput = document.querySelector('#total-expenses');
+const alertCtrl = document.querySelector('ion-alert-controller');
 
 let totalExpenses = 0;
 
@@ -21,6 +22,16 @@ confirmBtn.addEventListener('click', () => {
     enteredAmount <= 0 ||
     enteredAmount.trim().length <= 0
   ) {
+    alertCtrl
+      .create({
+        message: 'Please enter valid reason and amount!',
+        header: 'Invalid inputs',
+        buttons: ['Okay'],
+      })
+      .then((alertElement) => {
+        alertElement.present();
+      });
+
     return;
   }
   console.log(enteredReason, enteredAmount);
