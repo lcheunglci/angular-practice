c86e26831c5866d93aa1a4ab6fee238ac871cbdf
Lawrence LCI <lcheunglci@gmail.com>
2024-09-11 23:01:29 -0700

    Add tabs for offers and discover

--- ng-ionic-app/ionic-ng-booking/src/app/places/places-routing.module.ts
+++ ng-ionic-app/ionic-ng-booking/src/app/places/places-routing.module.ts
@@ -5,18 +5,78 @@ import { PlacesPage } from './places.page';
 
 const routes: Routes = [
   {
-    path: '',
-    component: PlacesPage
+    path: 'tabs',
+    component: PlacesPage,
+    children: [
+      {
+        path: 'discover',
+        children: [
+          {
+            path: '',
+            loadChildren: () =>
+              import('./discover/discover.module').then(
+                (m) => m.DiscoverPageModule
+              ),
+            // loadChildren: './discover/discover.module#DiscoverPageModule'
+          },
+          {
+            path: ':placeId',
+            loadChildren: () =>
+              import('./discover/place-detail/place-detail.module').then(
+                (m) => m.PlaceDetailPageModule
+              ),
+            // loadChildren: './discover/place-detail/place-detail.module#PlaceDetailPageModule'
+          },
+        ],
+      },
+      {
+        path: 'offers',
+        children: [
+          {
+            path: '',
+            loadChildren: () =>
+              import('./offers/offers.module').then((m) => m.OffersPageModule),
+            // loadChildren: './offers/offers.module#OffersPageModule
+          },
+          {
+            path: 'new',
+            loadChildren: () =>
+              import('./offers/new-offer/new-offer.module').then(
+                (m) => m.NewOfferPageModule
+              ),
+            // loadChildren: './offers/new-offer/new-offer.module#NewOfferPageModule
+          },
+          {
+            path: 'edit/:placeId',
+            loadChildren: () =>
+              import('./offers/edit-offer/edit-offer.module').then(
+                (m) => m.EditOfferPageModule
+              ),
+            // loadChildren: './offers/edit-offer/edit-offer.module#EditOfferPageModule
+          },
+          {
+            path: ':placeId',
+            loadChildren: () =>
+              import('./offers/offer-bookings/offer-bookings.module').then(
+                (m) => m.OfferBookingsPageModule
+              ),
+            // loadChildren: './offers/offer-bookings/offer-bookings.module#OfferBookingsPageModule
+          },
+        ],
+      },
+      {
+        path: '',
+        redirectTo: '/places/tabs/discover',
+        pathMatch: 'full'
+      },
+
+    ],
   },
   {
-    path: 'discover',
-    loadChildren: () => import('./discover/discover.module').then( m => m.DiscoverPageModule)
-  },  {
-    path: 'offers',
-    loadChildren: () => import('./offers/offers.module').then( m => m.OffersPageModule)
+    path: '',
+    redirectTo: '/places/tabs/discover',
+    pathMatch: 'full'
   }
-
-
 ];
 
 @NgModule({
