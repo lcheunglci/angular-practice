5d25f54f62d55a945a185f8fbc669836e92ffc3a
Lawrence LCI <lcheunglci@gmail.com>
2024-09-13 23:43:47 -0700

    Pass data to modal

--- ng-ionic-app/ionic-ng-booking/src/app/places/discover/place-detail/place-detail.page.ts
+++ ng-ionic-app/ionic-ng-booking/src/app/places/discover/place-detail/place-detail.page.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalController, NavController } from '@ionic/angular';
 import { CreateBookingComponent } from '../../../bookings/create-booking/create-booking.component';
+import { Place } from '../../place.model';
+import { PlacesService } from '../../places.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-place-detail',
@@ -8,12 +11,26 @@ import { CreateBookingComponent } from '../../../bookings/create-booking/create-
   styleUrls: ['./place-detail.page.scss'],
 })
 export class PlaceDetailPage implements OnInit {
+  place!: Place;
+
   constructor(
     private navCtrl: NavController,
+    private route: ActivatedRoute,
+    private placesService: PlacesService,
     private modalCtrl: ModalController
   ) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.route.paramMap.subscribe((paramMap) => {
+      if (!paramMap.has('placeId')) {
+        this.navCtrl.navigateBack('/places/tabs/discover');
+        return;
+      }
+
+      const placeId = paramMap.get('placeId') ?? '';
+      this.place = this.placesService.getPlace(placeId) as Place;
+    });
+  }
 
   onBookPlace() {
     // note the navigation with default router would show the wrong transition animation
@@ -21,9 +38,21 @@ export class PlaceDetailPage implements OnInit {
     // this.navCtrl.pop(); // only if there's previous page
     // this.navCtrl.navigateBack('/places/tabs/discover');
     this.modalCtrl
-      .create({ component: CreateBookingComponent })
+      .create({
+        component: CreateBookingComponent,
+        componentProps: {
+          selectedPlace: this.place,
+        },
+      })
       .then((modalEl) => {
         modalEl.present();
-      });
+        return modalEl.onDidDismiss();
+      })
+      .then(resultData => {
+        console.log(resultData.data, resultData.role);
+        if (resultData.role === 'confirm') {
+          console.log('BOOKED!');
+        }
+      })
   }
 }
