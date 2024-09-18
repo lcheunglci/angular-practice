d306fef4fb64ff051a7ac862528d80c90cca155a
Lawrence LCI <lcheunglci@gmail.com>
2024-09-17 23:31:44 -0700

    Add virtual scrolling

--- ng-ionic-app/ionic-ng-booking/src/app/places/discover/discover.page.ts
+++ ng-ionic-app/ionic-ng-booking/src/app/places/discover/discover.page.ts
@@ -9,11 +9,13 @@ import { Place } from '../place.model';
 })
 export class DiscoverPage implements OnInit {
   loadedPlaces: Place[] = [];
+  listedLoadedPlaces: Place[] = [];
   constructor(private placesService: PlacesService) { }
 
   ngOnInit() {
     // note: only fetch once due to caching
     this.loadedPlaces = this.placesService.places;
+    this.listedLoadedPlaces = this.loadedPlaces.slice(1);
   }
 
 }
