a0152bebb0d551be3c5358aad65e1e731d7ce6d8
Lawrence LCI <lcheunglci@gmail.com>
2024-09-24 23:42:50 -0700

    Add the date selector and update the place model with from and to

--- ng-ionic-app/ionic-ng-booking/src/app/places/places.service.ts
+++ ng-ionic-app/ionic-ng-booking/src/app/places/places.service.ts
@@ -11,23 +11,28 @@ export class PlacesService {
       'Manhattan Mansion',
       'In the heart of New York City',
       'https://unsplash.com/photos/UWYPTISci4o/download?ixid=M3wxMjA3fDB8MXxzZWFyY2h8MTB8fGZhbmN5JTIwbWFuc2lvbnxlbnwwfHx8fDE3MjYxMjQwNzl8MA&force=true&w=640',
-      149.99
+      149.99,
+      new Date('2024-01-01'),
+      new Date('2024-12-31')
     ),
     new Place(
       'p2',
-      'L\' Amour Toujours',
+      "L' Amour Toujours",
       'A romantic place in Paris!',
       'https://unsplash.com/photos/euNoSSVFl8U/download?ixid=M3wxMjA3fDB8MXxzZWFyY2h8MTF8fHBhcmlzJTIwaG91c2V8ZW58MHx8fHwxNzI2MTI0MTY1fDA&force=true&w=640',
-      189.99
+      189.99,
+      new Date('2024-01-01'),
+      new Date('2024-12-31')
     ),
     new Place(
       'p3',
       'The Foggy Palace',
       'Not your average city trip!',
       'https://unsplash.com/photos/nxc9KgICLRU/download?ixid=M3wxMjA3fDB8MXxzZWFyY2h8MTF8fGZvZ2d5JTIwcGFsYWNlfGVufDB8fHx8MTcyNjEyNDI1NHww&force=true&w=640',
-      99.99
+      99.99,
+      new Date('2024-01-01'),
+      new Date('2024-12-31')
     ),
-
   ];
 
   get places() {
@@ -36,7 +41,7 @@ export class PlacesService {
 
   constructor() {}
 
-  getPlace(id: string) : Place {
-    return {...this._places.find(p => p.id === id)} as Place;
+  getPlace(id: string): Place {
+    return { ...this._places.find((p) => p.id === id) } as Place;
   }
 }
