31477c209476e22012bb836ebae7a91cb769a159
Lawrence LCI <lcheunglci@gmail.com>
2024-09-30 23:52:12 -0700

    Add http client and use it for adding places

--- ng-ionic-app/ionic-ng-booking/src/app/places/places.service.ts
+++ ng-ionic-app/ionic-ng-booking/src/app/places/places.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Place } from './place.model';
 import { AuthService } from '../auth/auth.service';
-import { BehaviorSubject, delay, map, take, tap } from 'rxjs';
+import { BehaviorSubject, delay, map, switchMap, take, tap } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
@@ -44,7 +45,7 @@ export class PlacesService {
     return this._places.asObservable();
   }
 
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private http: HttpClient) {}
 
   getPlace(id: string) {
     return this.places.pipe(
@@ -62,6 +63,7 @@ export class PlacesService {
     dateFrom: Date,
     dateTo: Date
   ) {
+    let generatedId: string;
     const newPlace = new Place(
       Math.random().toString(),
       title,
@@ -73,11 +75,16 @@ export class PlacesService {
       this.authService.userId
     );
 
-    return this.places.pipe(
-      take(1),
-      delay(1000),
-      tap((places) => {
-        this._places.next(places.concat(newPlace));
+    return this.http.post<{name: string}>(process.env['DB_URL'] + 'offered-places.json' , {...newPlace, id: null })
+      .pipe(
+        switchMap(resData => {
+          generatedId = resData.name
+          return this.places;
+        }),
+        take(1),
+        tap((places) => {
+          newPlace.id = generatedId;
+          this._places.next(places.concat(newPlace));
       })
     );
   }
