ebd8c1e8206e9a04bfcfc9e4b7e8a488e86b3533
Lawrence LCI <lcheunglci@gmail.com>
2024-09-30 23:07:58 -0700

    Add android

--- ng-ionic-app/ionic-ng-booking/capacitor.config.ts
+++ ng-ionic-app/ionic-ng-booking/capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'ionic-ng-booking',
+  appId: 'com.lawrencelci.papayabnb',
+  appName: 'Papaya BnB',
   webDir: 'www'
 };
 
