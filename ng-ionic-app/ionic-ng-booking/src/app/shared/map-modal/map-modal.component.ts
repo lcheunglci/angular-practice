cd84e180da061a120e9eb5410d029d621337df61
Lawrence LCI <lcheunglci@gmail.com>
2024-10-15 23:54:10 -0700

    Hook up the google maps SDK

--- ng-ionic-app/ionic-ng-booking/src/app/shared/map-modal/map-modal.component.ts
+++ ng-ionic-app/ionic-ng-booking/src/app/shared/map-modal/map-modal.component.ts
@@ -1,17 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-map-modal',
   templateUrl: './map-modal.component.html',
   styleUrls: ['./map-modal.component.scss'],
 })
-export class MapModalComponent implements OnInit {
+export class MapModalComponent implements OnInit, AfterViewInit {
   constructor(private modalCtrl: ModalController) {}
+
+  ngAfterViewInit(): void {
+    this.getGoogleMaps().then(
+      googleMaps => {
+        
+      }
+    ).catch(err => {
+      console.log(err);
+    })
+  }
 
   ngOnInit() {}
 
   onCancel() {
     this.modalCtrl.dismiss();
+  }
+
+  private getGoogleMaps() {
+    const win = window as any;
+    const googleModule = win.google;
+    if (googleModule && googleModule.maps) {
+      return Promise.resolve(googleModule.maps);
+    }
+    return new Promise((resolve, reject) => {
+      const script = document.createElement('script');
+      script.src = 'http://maps.googleapis.com/maps/api/js?key=' + environment.GOOGLE_MAP_API_KEY;
+      script.async = true;
+      script.defer = true;
+      document.body.appendChild(script);
+      script.onload = () =>  {
+        const loadedGoogleModule = win.google;
+        if (loadedGoogleModule && loadedGoogleModule.maps) {
+          resolve(loadedGoogleModule.maps);
+        } else {
+          reject('Google maps SDK not available');
+        }
+      }
+    })
   }
 }
--- ng-ionic-app/ionic-ng-booking/src/environments/environment.prod.ts
+++ ng-ionic-app/ionic-ng-booking/src/environments/environment.prod.ts
@@ -4,4 +4,5 @@ export const environment = {
   AUTH_SIGN_UP_URL: process.env['NG_APP_FB_AUTH_SIGNUP_URL'],
   AUTH_SIGN_IN_URL: process.env['NG_APP_FB_AUTH_SIGNIN_URL'],
   FB_API_KEY: process.env['NG_APP_FB_AUTH_API'],
+  GOOGLE_MAP_API_KEY: process.env['NG_GOOGLE_MAPS_API']
 };
