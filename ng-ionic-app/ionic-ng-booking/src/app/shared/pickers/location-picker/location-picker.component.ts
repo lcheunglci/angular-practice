e1aef102af645d8340dbdc8ba31c612ae2359b60
Lawrence LCI <lcheunglci@gmail.com>
2024-10-18 23:54:53 -0700

    Add logic to get the address with geocode api

--- ng-ionic-app/ionic-ng-booking/src/app/shared/pickers/location-picker/location-picker.component.ts
+++ ng-ionic-app/ionic-ng-booking/src/app/shared/pickers/location-picker/location-picker.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 import { MapModalComponent } from '../../map-modal/map-modal.component';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-location-picker',
@@ -8,16 +11,40 @@ import { MapModalComponent } from '../../map-modal/map-modal.component';
   styleUrls: ['./location-picker.component.scss'],
 })
 export class LocationPickerComponent implements OnInit {
-  constructor(private modalCtrl: ModalController) {}
+  constructor(private modalCtrl: ModalController, private http: HttpClient) {}
 
   ngOnInit() {}
 
   onPickLocation() {
     this.modalCtrl.create({ component: MapModalComponent }).then((modalEl) => {
       modalEl.onDidDismiss().then((modalData) => {
-        console.log(modalData.data);
+        if (!modalData.data) {
+          return;
+        }
+
+        this.getAddress(modalData.data.lat, modalData.data.lng).subscribe(
+          (address) => {
+            console.log(address);
+          }
+        );
       });
       modalEl.present();
     });
+  }
+
+  private getAddress(lat: number, lng: number) {
+    return this.http
+      .get<any>(
+        `https:googleapis.com/map/api/geocode/json?latlng=${lat},${lng}&key=${environment.GOOGLE_MAP_API_KEY}`
+      )
+      .pipe(
+        map((geoData) => {
+          if (!geoData || !geoData.results || geoData.results.length === 0) {
+            return null;
+          }
+
+          return geoData.results[0].formatted_address;
+        })
+      );
   }
 }
