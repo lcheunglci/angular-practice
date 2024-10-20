0299a8965a5bc0af4a0765927a8478e89448f1af
Lawrence LCI <lcheunglci@gmail.com>
2024-10-19 23:45:07 -0700

    Use the staticmap  api to populate the static map image when selecting a place

--- ng-ionic-app/ionic-ng-booking/src/app/shared/pickers/location-picker/location-picker.component.ts
+++ ng-ionic-app/ionic-ng-booking/src/app/shared/pickers/location-picker/location-picker.component.ts
@@ -3,7 +3,8 @@ import { ModalController } from '@ionic/angular';
 import { MapModalComponent } from '../../map-modal/map-modal.component';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
-import { map } from 'rxjs';
+import { map, of, switchMap } from 'rxjs';
+import { PlaceLocation } from 'src/app/places/location.model';
 
 @Component({
   selector: 'app-location-picker',
@@ -22,11 +23,26 @@ export class LocationPickerComponent implements OnInit {
           return;
         }
 
-        this.getAddress(modalData.data.lat, modalData.data.lng).subscribe(
-          (address) => {
-            console.log(address);
-          }
-        );
+        const pickedLocation: PlaceLocation = {
+          lat: modalData.data.lat,
+          lng: modalData.data.lng,
+          address: '',
+          staticMapImageUrl: '',
+        };
+
+        this.getAddress(modalData.data.lat, modalData.data.lng)
+          .pipe(
+            switchMap((address) => {
+              pickedLocation.address = address;
+
+              return of(
+                this.getMapImage(pickedLocation.lat, pickedLocation.lng, 14)
+              );
+            })
+          )
+          .subscribe((staticMapImageUrl) => {
+            pickedLocation.staticMapImageUrl = staticMapImageUrl;
+          });
       });
       modalEl.present();
     });
@@ -46,5 +62,11 @@ export class LocationPickerComponent implements OnInit {
           return geoData.results[0].formatted_address;
         })
       );
+  }
+
+  private getMapImage(lat: number, lng: number, zoom: number) {
+    return `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&zoom=${zoom}
+      &size=500x300&maptype=roadmap&markers=color:red%7Clabel:Place%7C${lat},${lng}
+      &key=${environment.GOOGLE_MAP_API_KEY}`;
   }
 }
