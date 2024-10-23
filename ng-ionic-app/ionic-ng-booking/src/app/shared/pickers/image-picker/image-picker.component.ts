00a1eb6d59541ed99fe2198645ae2b0a2d22e7e8
Lawrence LCI <lcheunglci@gmail.com>
2024-10-22 23:11:15 -0700

    Add logs to see the platform

--- ng-ionic-app/ionic-ng-booking/src/app/shared/pickers/image-picker/image-picker.component.ts
+++ ng-ionic-app/ionic-ng-booking/src/app/shared/pickers/image-picker/image-picker.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
 import { Capacitor } from '@capacitor/core';
+import { Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-image-picker',
@@ -9,11 +10,17 @@ import { Capacitor } from '@capacitor/core';
 })
 export class ImagePickerComponent implements OnInit {
   @Output() imagePick = new EventEmitter<string>();
-  selectedImage: string|undefined = '';
+  selectedImage: string | undefined = '';
 
-  constructor() {}
+  constructor(private platform: Platform) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    console.log('Mobile', this.platform.is('mobile'));
+    console.log('Hybrid', this.platform.is('hybrid'));
+    console.log('iOS', this.platform.is('ios'));
+    console.log('Android', this.platform.is('android'));
+    console.log('Desktop', this.platform.is('desktop'));
+  }
 
   onPickImage() {
     if (Capacitor.isPluginAvailable('Camera')) {
@@ -25,13 +32,15 @@ export class ImagePickerComponent implements OnInit {
       correctOrientation: true,
       height: 320,
       width: 200,
-      resultType: CameraResultType.Base64
-    }).then(image => {
-      this.selectedImage = image.base64String;
-      this.imagePick.emit(image.base64String);
-    }).catch(error => {
-      console.log(error);
-      return false;
+      resultType: CameraResultType.Base64,
     })
+      .then((image) => {
+        this.selectedImage = image.base64String;
+        this.imagePick.emit(image.base64String);
+      })
+      .catch((error) => {
+        console.log(error);
+        return false;
+      });
   }
 }
