1f9eda35b1b706aa2ccc87eadaada1dae5e8caef
Lawrence LCI <lcheunglci@gmail.com>
2024-10-24 00:03:09 -0700

    Update the form to set the image file

--- ng-ionic-app/ionic-ng-booking/src/app/places/offers/new-offer/new-offer.page.ts
+++ ng-ionic-app/ionic-ng-booking/src/app/places/offers/new-offer/new-offer.page.ts
@@ -5,12 +5,12 @@ import { Router } from '@angular/router';
 import { LoadingController } from '@ionic/angular';
 import { PlaceLocation } from '../../location.model';
 
-function base64ToBlog(base64Data: string, contentType: any) {
+function base64ToBlog(base64Data: string, contentType: any): File {
   contentType = contentType || '';
   const sliceSize = 1024;
   const byteCharacters = window.atob(base64Data);
   const bytesLength = byteCharacters.length;
-  const slicesCount = Math.ceil(bytesLength/sliceSize);
+  const slicesCount = Math.ceil(bytesLength / sliceSize);
   const byteArrays = new Array(slicesCount);
 
   for (let sliceIndex = 0; sliceIndex < slicesCount; ++sliceIndex) {
@@ -23,7 +23,7 @@ function base64ToBlog(base64Data: string, contentType: any) {
     }
     byteArrays[sliceIndex] = new Uint8Array(bytes);
   }
-  return new Blob(byteArrays, {type: contentType});
+  return new Blob(byteArrays, { type: contentType });
 }
 
 @Component({
@@ -63,7 +63,7 @@ export class NewOfferPage implements OnInit {
         validators: [Validators.required],
       }),
       location: new FormControl(null, { validators: [Validators.required] }),
-      image: new FormControl(null, { validators: [Validators.required] }),
+      image: new FormControl(null),
     });
   }
 
@@ -100,17 +100,21 @@ export class NewOfferPage implements OnInit {
   }
 
   onImagePicked(imageData: string | File) {
+    let imageFile: File | Blob;
     if (typeof imageData === 'string') {
       try {
-        const imageFile = base64ToBlog(imageData.replace('data:image/jpeg;base64,',''), 'image/jpeg');
-      } catch( error ) {
+        imageFile = base64ToBlog(
+          imageData.replace('data:image/jpeg;base64,', ''),
+          'image/jpeg'
+        );
+      } catch (error) {
         console.log(error);
         return;
       }
-      
-
     } else {
-
+      imageFile = imageData;
     }
+
+    this.form.patchValue({ image: imageFile });
   }
 }
