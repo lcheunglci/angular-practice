9b437732e1d46630d2849c7803bbc9536cf86dfd
Lawrence LCI <lcheunglci@gmail.com>
2024-11-05 00:00:40 -0800

    Refactor side nav and header into its own component

--- ng-fitness-tracker/src/app/app.module.ts
+++ ng-fitness-tracker/src/app/app.module.ts
@@ -13,6 +13,8 @@ import { NewTrainingComponent } from './training/new-training/new-training.compo
 import { PastTrainingsComponent } from './training/past-trainings/past-trainings.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { FormsModule } from '@angular/forms';
+import { HeaderComponent } from './navigation/header/header.component';
+import { SidenavListComponent } from './navigation/sidenav-list/sidenav-list.component';
 
 @NgModule({
   declarations: [
@@ -23,7 +25,9 @@ import { FormsModule } from '@angular/forms';
     CurrentTrainingComponent,
     NewTrainingComponent,
     PastTrainingsComponent,
-    WelcomeComponent
+    WelcomeComponent,
+    HeaderComponent,
+    SidenavListComponent
   ],
   imports: [
     BrowserModule,
