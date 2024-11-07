aa43b73d39d253da1c85aac9a8afa517ee14a5f0
Lawrence LCI <lcheunglci@gmail.com>
2024-11-06 22:42:54 -0800

    Add card to new training page

--- ng-fitness-tracker/src/app/material.module.ts
+++ ng-fitness-tracker/src/app/material.module.ts
@@ -9,6 +9,7 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatListModule } from '@angular/material/list';
 import { MatTabsModule } from '@angular/material/tabs';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   imports: [
@@ -22,6 +23,7 @@ import { MatTabsModule } from '@angular/material/tabs';
     MatToolbarModule,
     MatListModule,
     MatTabsModule,
+    MatCardModule,
   ],
   exports: [
     MatButtonModule,
@@ -34,6 +36,7 @@ import { MatTabsModule } from '@angular/material/tabs';
     MatToolbarModule,
     MatListModule,
     MatTabsModule,
+    MatCardModule,
   ],
 })
 export class MaterialModule {}
