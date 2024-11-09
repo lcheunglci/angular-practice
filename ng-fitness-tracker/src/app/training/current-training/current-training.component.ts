0608ab1ac7f96db9053a9192444ab0f908f8af3a
Lawrence LCI <lcheunglci@gmail.com>
2024-11-08 19:00:37 -0800

    Prevent progress to go above 100% and add the stop timer in current training

--- ng-fitness-tracker/src/app/training/current-training/current-training.component.ts
+++ ng-fitness-tracker/src/app/training/current-training/current-training.component.ts
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CurrentTrainingComponent implements OnInit {
   progress = 0;
+  timer: any = null;
+
   ngOnInit(): void {
-    setInterval( () => {
+    this.timer = setInterval( () => {
       this.progress = this.progress + 5;
+      if (this.progress >= 100) {
+        clearInterval(this.timer);
+      }
     },1000)
+  }
+
+  onStop() {
+    if (this.timer) {
+      clearInterval(this.timer);
+    }
   }
 }
