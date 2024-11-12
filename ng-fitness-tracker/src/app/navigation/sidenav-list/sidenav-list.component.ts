3b56c4c0282ed6fd642eb4fb789a9472a8e69c5a
Lawrence LCI <lcheunglci@gmail.com>
2024-11-11 21:59:05 -0800

    Update the side nav menu to reflect the auth status

--- ng-fitness-tracker/src/app/navigation/sidenav-list/sidenav-list.component.ts
+++ ng-fitness-tracker/src/app/navigation/sidenav-list/sidenav-list.component.ts
@@ -1,12 +1,38 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { AuthService } from './../../auth/auth.service';
+import {
+  Component,
+  EventEmitter,
+  OnDestroy,
+  OnInit,
+  Output,
+} from '@angular/core';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-sidenav-list',
   templateUrl: './sidenav-list.component.html',
   styleUrl: './sidenav-list.component.css',
 })
-export class SidenavListComponent {
+export class SidenavListComponent implements OnInit, OnDestroy {
   @Output() closeSideNav = new EventEmitter<void>();
+  isAuth = false;
+  authSubscription!: Subscription;
+
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.authSubscription = this.authService.authChange.subscribe(
+      (authStatus) => {
+        this.isAuth = authStatus;
+      }
+    );
+  }
+
+  ngOnDestroy(): void {
+    if (this.authSubscription) {
+      this.authSubscription.unsubscribe();
+    }
+  }
 
   onClose() {
     this.closeSideNav.emit();
