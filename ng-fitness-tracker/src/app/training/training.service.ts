f027ee038cb80927fdd61420fc5e479aea2412a5
Lawrence LCI <lcheunglci@gmail.com>
2024-11-12 23:19:04 -0800

    Add training service

---
+++ ng-fitness-tracker/src/app/training/training.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { Exercise } from './exercise.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TrainingService {
+  availableExercises: Exercise[] = [
+    {
+      id: 'crunches',
+      name: 'Crunches',
+      duration: 30,
+      calories: 8,
+      state: null,
+    },
+    {
+      id: 'touch-toes',
+      name: 'Touch Toes',
+      duration: 180,
+      calories: 15,
+      state: null,
+    },
+    {
+      id: 'side-lunges',
+      name: 'Side Lunges',
+      duration: 120,
+      calories: 18,
+      state: null,
+    },
+    { id: 'burpees', name: 'Burpees', duration: 60, calories: 8, state: null },
+  ];
+
+  constructor() {}
+}
