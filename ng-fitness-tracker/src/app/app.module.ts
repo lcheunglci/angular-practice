fc02063cf89841e6fc6ea9a57e6fe92ed4ff404a
Lawrence LCI <lcheunglci@gmail.com>
2024-11-19 00:43:41 -0800

    Switch custom webpack with ngx-env

--- ng-fitness-tracker/src/app/app.module.ts
+++ ng-fitness-tracker/src/app/app.module.ts
@@ -20,6 +20,7 @@ import { AuthService } from './auth/auth.service';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -44,7 +45,7 @@ import { getFirestore, provideFirestore } from '@angular/fire/firestore';
   ],
   providers: [
     provideAnimationsAsync(),
-    provideFirebaseApp(() => initializeApp(environment.firebase.config)),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideAuth(() => getAuth()),
     provideFirestore(() => getFirestore()),
   ],
