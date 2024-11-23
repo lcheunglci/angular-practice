aa54308f5c2fa788eb693b92a56fa0074cdc5c82
Lawrence LCI <lcheunglci@gmail.com>
2024-11-22 23:51:55 -0800

    Add real auth from firebase to the auth service

--- ng-fitness-tracker/src/app/auth/auth.service.ts
+++ ng-fitness-tracker/src/app/auth/auth.service.ts
@@ -2,6 +2,8 @@ import { Injectable, signal } from '@angular/core';
 import { User } from './user.model';
 import { AuthData } from './auth-data.model';
 import { Subject } from 'rxjs';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root',
@@ -11,24 +13,47 @@ export class AuthService {
   authChange = new Subject<boolean>();
   authenticated = signal(false);
 
-  constructor() {}
+  constructor(private router: Router, private fireAuth: AngularFireAuth) {}
 
   registerUser(authData: AuthData) {
-    this.user = {
-      email: authData.email,
-      userId: Math.round(Math.random() * 10000).toString(),
-    };
+    this.fireAuth
+      .createUserWithEmailAndPassword(authData.email, authData.password)
+      .then((results) => {
+        console.log(results);
+        this.authSuccessfully();
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+
+    // this.user = {
+    //   email: authData.email,
+    //   userId: Math.round(Math.random() * 10000).toString(),
+    // };
+    // this.authSuccessfully();
+  }
+
+  private authSuccessfully() {
     this.authChange.next(true);
     this.authenticated.set(true);
   }
 
   login(authData: AuthData) {
-    this.user = {
-      email: authData.email,
-      userId: Math.round(Math.random() * 10000).toString(),
-    };
-    this.authChange.next(true);
-    this.authenticated.set(true);
+    // this.user = {
+    //   email: authData.email,
+    //   userId: Math.round(Math.random() * 10000).toString(),
+    // };
+    this.fireAuth
+      .signInWithEmailAndPassword(authData.email, authData.password)
+      .then((results) => {
+        console.log(results);
+        this.authSuccessfully();
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+
+    this.authSuccessfully();
   }
 
   logout() {
