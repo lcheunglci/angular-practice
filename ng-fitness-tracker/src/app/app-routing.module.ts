31db8daeb7a6b304bd48edad953fb009c90daf3f
Lawrence LCI <lcheunglci@gmail.com>
2024-11-28 00:12:36 -0800

    Add auth routing

--- ng-fitness-tracker/src/app/app-routing.module.ts
+++ ng-fitness-tracker/src/app/app-routing.module.ts
@@ -1,15 +1,11 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes, CanActivateFn } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { WelcomeComponent } from './welcome/welcome.component';
-import { SignupComponent } from './auth/signup/signup.component';
-import { LoginComponent } from './auth/login/login.component';
 import { TrainingComponent } from './training/training.component';
 import { authGuard } from './auth/auth.guard';
 
 const routes: Routes = [
   { path: '', component: WelcomeComponent },
-  { path: 'signup', component: SignupComponent },
-  { path: 'login', component: LoginComponent },
   { path: 'training', component: TrainingComponent, canActivate: [authGuard] },
 ];
 
--- ng-fitness-tracker/src/app/auth/auth.module.ts
+++ ng-fitness-tracker/src/app/auth/auth.module.ts
@@ -4,9 +4,15 @@ import { LoginComponent } from './login/login.component';
 import { MaterialModule } from '../material.module';
 import { ReactiveFormsModule } from '@angular/forms';
 import { SharedModule } from '../shared/shared.module';
+import { AuthRoutingModule } from './auth-routing.module';
 
 @NgModule({
   declarations: [SignupComponent, LoginComponent],
-  imports: [ReactiveFormsModule, MaterialModule, SharedModule],
+  imports: [
+    ReactiveFormsModule,
+    MaterialModule,
+    SharedModule,
+    AuthRoutingModule,
+  ],
 })
 export class AuthModule {}
