7d65046cba5db65e022baccba2f1068c28f7c7f4
Lawrence LCI <lcheunglci@gmail.com>
2024-11-27 23:55:53 -0800

    Add shared module

--- ng-fitness-tracker/src/app/auth/auth.module.ts
+++ ng-fitness-tracker/src/app/auth/auth.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { SignupComponent } from './signup/signup.component';
 import { LoginComponent } from './login/login.component';
 import { MaterialModule } from '../material.module';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [SignupComponent, LoginComponent],
-  imports: [CommonModule, FormsModule, ReactiveFormsModule, MaterialModule],
+  imports: [ReactiveFormsModule, MaterialModule, SharedModule],
 })
 export class AuthModule {}
