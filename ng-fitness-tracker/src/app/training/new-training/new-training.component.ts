2b4014a65ea95ad499338a70ee2360a0fa453d68
Lawrence LCI <lcheunglci@gmail.com>
2024-12-05 23:16:54 -0800

    switch the new training to use the NgRx store

--- ng-fitness-tracker/src/app/training/new-training/new-training.component.ts
+++ ng-fitness-tracker/src/app/training/new-training/new-training.component.ts
@@ -1,54 +1,34 @@
+import { reducers } from './../../app.reducer';
 import { Exercise } from './../exercise.model';
-import {
-  Component,
-  EventEmitter,
-  OnDestroy,
-  OnInit,
-  Output,
-} from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { TrainingService } from '../training.service';
 import { NgForm } from '@angular/forms';
-import { Observable, Subscription } from 'rxjs';
+import { from, Observable } from 'rxjs';
 import { UIService } from '../../shared/ui.service';
+import { Store } from '@ngrx/store';
+import * as fromTraining from '../training.reducer';
+import * as fromRoot from '../../app.reducer';
 
 @Component({
   selector: 'app-new-training',
   templateUrl: './new-training.component.html',
   styleUrl: './new-training.component.css',
 })
-export class NewTrainingComponent implements OnInit, OnDestroy {
+export class NewTrainingComponent implements OnInit {
   @Output() trainingStart = new EventEmitter<void>();
-  exercises!: Exercise[];
-  exerciseSub!: Subscription;
-  loadingSub!: Subscription;
-  isLoading = false;
+  exercises$!: Observable<Exercise[]>;
+  isLoading$!: Observable<boolean>;
 
   constructor(
     private trainingService: TrainingService,
-    private uiService: UIService
+    private uiService: UIService,
+    private store: Store<fromTraining.State>
   ) {}
 
-  ngOnDestroy(): void {
-    if (this.exerciseSub) {
-      this.exerciseSub.unsubscribe();
-    }
-    if (this.loadingSub) {
-      this.loadingSub.unsubscribe();
-    }
-  }
-
   ngOnInit(): void {
-    this.loadingSub = this.uiService.loadingStateChanged.subscribe(
-      (isLoading) => {
-        this.isLoading = isLoading;
-      }
-    );
-    this.exerciseSub = this.trainingService.exercisesChanged.subscribe(
-      (exercises) => {
-        this.exercises = exercises;
-        this.isLoading = false;
-      }
-    );
+    this.isLoading$ = this.store.select(fromRoot.getIsLoading);
+    this.exercises$ = this.store.select(fromTraining.getAvailableExercises);
+
     this.fetchExercises();
   }
 
