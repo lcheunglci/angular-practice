aea4018cc443d5eeff836655b883ed980f2e2871
Lawrence LCI <lcheunglci@gmail.com>
2024-12-13 23:59:04 -0800

    Add the 4 diff tabs

--- ng-ionic-app/ionic-delivery-app/src/app/pages/tabs/tabs-routing.module.ts
+++ ng-ionic-app/ionic-delivery-app/src/app/pages/tabs/tabs-routing.module.ts
@@ -7,7 +7,23 @@ const routes: Routes = [
   {
     path: '',
     component: TabsPage
+  },  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: 'search',
+    loadChildren: () => import('./search/search.module').then( m => m.SearchPageModule)
+  },
+  {
+    path: 'cart',
+    loadChildren: () => import('./cart/cart.module').then( m => m.CartPageModule)
+  },
+  {
+    path: 'account',
+    loadChildren: () => import('./account/account.module').then( m => m.AccountPageModule)
   }
+
 ];
 
 @NgModule({
