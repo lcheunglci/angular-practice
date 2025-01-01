aa9c900830e64fea8fcfcbcdf9a2a1685a7ab2c5
Lawrence LCI <lcheunglci@gmail.com>
2024-12-31 22:03:02 -0800

    Use the productService to populate the products in the list

--- rxjs-practice/src/app/products/product-list/product-list.component.ts
+++ rxjs-practice/src/app/products/product-list/product-list.component.ts
@@ -1,25 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
 
 import { NgIf, NgFor, NgClass } from '@angular/common';
 import { Product } from '../product';
 import { ProductDetailComponent } from '../product-detail/product-detail.component';
+import { ProductService } from '../product.service';
+import { Subscription, tap } from 'rxjs';
 
 @Component({
-    selector: 'pm-product-list',
-    templateUrl: './product-list.component.html',
-    standalone: true,
-  imports: [NgIf, NgFor, NgClass, ProductDetailComponent]
+  selector: 'pm-product-list',
+  templateUrl: './product-list.component.html',
+  standalone: true,
+  imports: [NgIf, NgFor, NgClass, ProductDetailComponent],
 })
-export class ProductListComponent {
-  // Just enough here for the template to compile
+export class ProductListComponent implements OnInit, OnDestroy {
   pageTitle = 'Products';
   errorMessage = '';
+  productService = inject(ProductService);
+  sub!: Subscription;
 
   // Products
   products: Product[] = [];
 
   // Selected product id to highlight the entry
   selectedProductId: number = 0;
+
+  ngOnInit(): void {
+    this.sub = this.productService
+      .getProducts()
+      .pipe(tap(() => console.log('In component pipeline')))
+      .subscribe((products) => {
+        this.products = products;
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
+  }
 
   onSelected(productId: number): void {
     this.selectedProductId = productId;
