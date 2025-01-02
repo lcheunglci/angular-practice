b2b010958498f0f1cc597cb8a84dbae3cf62e8e5
Lawrence LCI <lcheunglci@gmail.com>
2025-01-01 23:35:48 -0800

    Add error handling in when the get products fails

--- rxjs-practice/src/app/products/product.service.ts
+++ rxjs-practice/src/app/products/product.service.ts
@@ -1,7 +1,9 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
-import { Observable, tap } from 'rxjs';
+import { catchError, Observable, of, tap, throwError } from 'rxjs';
 import { Product } from './product';
+import { ProductData } from './product-data';
+import { HttpErrorService } from '../utilities/http-error.service';
 
 @Injectable({
   providedIn: 'root',
@@ -10,12 +12,18 @@ export class ProductService {
   private productsUrl = 'api/products';
 
   private http = inject(HttpClient);
+  private errorService = inject(HttpErrorService);
 
   getProducts(): Observable<Product[]> {
     return this.http.get<Product[]>(this.productsUrl).pipe(
       tap((next) => {
         console.log('In http.get pipeline');
-      })
+      }),
+      catchError(this.handleError)
+      // catchError((error) => {
+      //   console.error(error);
+      //   return of(ProductData.products);
+      // })
     );
   }
 
@@ -24,5 +32,11 @@ export class ProductService {
     return this.http
       .get<Product>(productUrl)
       .pipe(tap(() => console.log('In http.get by id pipeline')));
+  }
+
+  private handleError(err: HttpErrorResponse): Observable<never> {
+    const formattedMessage = this.errorService.formatError(err);
+    return throwError(() => formattedMessage);
+    // throw formattedMessage;
   }
 }
