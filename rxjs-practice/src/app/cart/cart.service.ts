7bf69588d03e57d436e1b68be866b2c98e912436
Lawrence LCI <lcheunglci@gmail.com>
2025-01-10 23:10:37 -0800

    Use signal to add to cart

--- rxjs-practice/src/app/cart/cart.service.ts
+++ rxjs-practice/src/app/cart/cart.service.ts
@@ -1,9 +1,18 @@
-import { Injectable, signal } from '@angular/core';
+import { effect, Injectable, signal } from '@angular/core';
 import { CartItem } from './cart';
+import { Product } from '../products/product';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CartService {
-  carItems = signal<CartItem[]>([]);
+  cartItems = signal<CartItem[]>([]);
+
+  eLength = effect(() => 'Cart array length: ' + this.cartItems().length);
+
+  addToCart(product: Product) {
+    // note: this does will not make the notification when the cartItems array is updated, use update or set instead.
+    // this.cartItems().push({ product, quantity: 1 });
+    this.cartItems.update((items) => [...items, { product, quantity: 1 }]);
+  }
 }
