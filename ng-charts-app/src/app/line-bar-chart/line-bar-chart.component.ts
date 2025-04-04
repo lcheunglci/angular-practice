f411fd71f032e784d474b90838cc62c6b258b3f2
Lawrence LCI <lcheunglci@gmail.com>
2025-04-03 23:48:49 -0700

    Add new project for ng2-charts app

---
+++ ng-charts-app/src/app/line-bar-chart/line-bar-chart.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-line-bar-chart',
+  templateUrl: './line-bar-chart.component.html',
+  styleUrl: './line-bar-chart.component.css'
+})
+export class LineBarChartComponent {
+
+}
---
+++ ng-charts-app/src/app/pie-doughnut-chart/pie-doughnut-chart.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PieDoughnutChartComponent } from './pie-doughnut-chart.component';
+
+describe('PieDoughnutChartComponent', () => {
+  let component: PieDoughnutChartComponent;
+  let fixture: ComponentFixture<PieDoughnutChartComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [PieDoughnutChartComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(PieDoughnutChartComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
