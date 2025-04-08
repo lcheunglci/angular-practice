5efac8ef7a957551788ab622760a517ca959ea1a
Lawrence LCI <lcheunglci@gmail.com>
2025-04-07 22:58:14 -0700

    Add multi-layer dataset to the doughnut and pie charts

--- ng-charts-app/src/app/pie-doughnut-chart/pie-doughnut-chart.component.ts
+++ ng-charts-app/src/app/pie-doughnut-chart/pie-doughnut-chart.component.ts
@@ -11,13 +11,22 @@ export class PieDoughnutChartComponent {
 
   labels = ['YES', 'NO', 'N/A'];
 
-  dataset1 = [80, 15, 5];
+  // dataset1 = [80, 15, 5];
+  dataset1 = [350, 120, 100];
+  dataset2 = [50, 150, 120];
+  dataset3 = [250, 130, 70];
 
   doughnutChartData: ChartConfiguration<'doughnut'>['data'] = {
     labels: this.labels,
     datasets: [
       {
         data: this.dataset1,
+      },
+      {
+        data: this.dataset2,
+      },
+      {
+        data: this.dataset3,
       },
     ],
   };
@@ -27,6 +36,12 @@ export class PieDoughnutChartComponent {
     datasets: [
       {
         data: this.dataset1,
+      },
+      {
+        data: this.dataset2,
+      },
+      {
+        data: this.dataset3,
       },
     ],
   };
