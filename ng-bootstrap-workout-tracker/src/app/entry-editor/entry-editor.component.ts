478f63d90f80d1aa8fef60902faf38f78cb8fce0
Lawrence LCI <lcheunglci@gmail.com>
2025-04-12 23:48:28 -0700

    Enhancement to the date picker to have a max and start date

--- ng-bootstrap-workout-tracker/src/app/entry-editor/entry-editor.component.ts
+++ ng-bootstrap-workout-tracker/src/app/entry-editor/entry-editor.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { WorkoutApiService } from '../services/workout-api.service';
+import { NgbDate, NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-entry-editor',
@@ -10,12 +11,21 @@ import { WorkoutApiService } from '../services/workout-api.service';
 export class EntryEditorComponent implements OnInit {
   public workout: any = {};
   public loading = false;
+  public startDate: any;
+  public maxDate: NgbDateStruct;
 
   constructor(
     private router: ActivatedRoute,
     private nav: Router,
     private api: WorkoutApiService
-  ) {}
+  ) {
+    const today = new Date();
+    this.maxDate = NgbDate.from({
+      year: today.getFullYear(),
+      month: today.getMonth() + 1,
+      day: today.getDate() + 1,
+    }) as NgbDateStruct;
+  }
 
   ngOnInit(): void {
     this.router.params.subscribe((params) => {
@@ -24,6 +34,8 @@ export class EntryEditorComponent implements OnInit {
         this.loading = true;
         this.api.getWorkout(id).subscribe((data) => {
           this.workout = data;
+          let d = new Date(this.workout.date);
+          this.startDate = { year: d.getFullYear(), month: d.getMonth() + 1 };
           this.loading = false;
         });
       }
