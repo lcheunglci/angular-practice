e35cb6050bddcddaa6c5c67522c95cf6f08b951a
Lawrence LCI <lcheunglci@gmail.com>
2025-04-16 21:03:49 -0700

    Add validation to enforce the flow

--- ng-bootstrap-workout-tracker/src/app/home/home.component.ts
+++ ng-bootstrap-workout-tracker/src/app/home/home.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
+  public stepComplete = 0;
+  @ViewChild('acc') acc: any;
 
+  nextButton(step: number) {
+    this.stepComplete = step;
+    this.acc.toggle(`panel${this.stepComplete + 1}`);
+  }
+
+  beforeChange($event: any) {
+    if ($event.panelId == 'panel2' && this.stepComplete < 1) {
+      $event.preventDefault();
+    }
+    if ($event.panelId == 'panel3' && this.stepComplete < 2) {
+      $event.preventDefault();
+    }
+  }
 }
