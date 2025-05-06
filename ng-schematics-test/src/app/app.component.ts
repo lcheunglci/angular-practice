c1cf3e3bb5f623145ddcb19218a24a76d295e461
Lawrence LCI <lcheunglci@gmail.com>
2025-05-05 22:56:52 -0700

    Add a material-table component using the schematics

--- ng-schematics-test/src/app/app.component.ts
+++ ng-schematics-test/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MyDashComponent } from './my-dash/my-dash.component';
+import { MyTableComponent } from './my-table/my-table.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MyDashComponent],
+  imports: [RouterOutlet, MyDashComponent, MyTableComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'ng-schematics-test';
