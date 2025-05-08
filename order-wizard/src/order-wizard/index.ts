37f15732a34c27bc7394ce779022f6770b311667
Lawrence LCI <lcheunglci@gmail.com>
2025-05-07 23:23:14 -0700

    Add template files to the order-wizard schematic

--- order-wizard/src/order-wizard/index.ts
+++ order-wizard/src/order-wizard/index.ts
@@ -1,10 +1,26 @@
-import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
+import { normalize } from '@angular-devkit/core';
+import {
+  apply,
+  MergeStrategy,
+  mergeWith,
+  move,
+  Rule,
+  SchematicContext,
+  Tree,
+  url,
+} from '@angular-devkit/schematics';
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
 export function orderWizard(_options: any): Rule {
-  console.log(_options);
   return (tree: Tree, _context: SchematicContext) => {
-    return tree;
+    const folderPath = normalize(_options.path + '/' + _options.name);
+    let files = url('./files');
+
+    const newTree = apply(files, [move(folderPath)]);
+
+    const templateRule = mergeWith(newTree, MergeStrategy.Default);
+
+    return templateRule(tree, _context);
   };
 }
