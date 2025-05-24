eb50c84434cf4f6261fc78b1e61c5fb2fc82eb7a
Lawrence LCI <lcheunglci@gmail.com>
2025-05-23 18:01:29 -0700

    Move the rating from book list to the library

--- ng-lib-examples/example-app/ng-material-frontend/projects/angular-rating/src/lib/angular-rating.component.ts
+++ ng-lib-examples/example-app/ng-material-frontend/projects/angular-rating/src/lib/angular-rating.component.ts
@@ -1,16 +1,45 @@
-import { Component } from '@angular/core';
+import { NgClass, NgFor } from '@angular/common';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'lib-angular-rating',
   standalone: true,
-  imports: [],
+  imports: [NgClass, NgFor],
   template: `
-    <p>
-      angular-rating works!
-    </p>
+    <span class="rating-wrapper">
+      <span *ngFor="let option of options">
+        <span
+          (click)="changeRating(option)"
+          [ngClass]="{ rated: option <= rating }"
+          >&#9733;</span
+        >
+      </span>
+    </span>
   `,
-  styles: ``
-})
-export class AngularRatingComponent {
+  styles: `  
+  .rated {
+    color: orange;
+  }
 
+  .rating-wrapper {
+    cursor: pointer;
+  }
+`,
+})
+export class AngularRatingComponent implements OnInit {
+  @Output() ratingChanged = new EventEmitter();
+  @Input() rating = 0;
+  @Input() count = 0;
+
+  options: number[] = [];
+
+  ngOnInit(): void {
+    // create an array with 'count' items
+    // increase every item by one
+    this.options = Array.from({ length: this.count }, (v, k) => ++k);
+  }
+
+  changeRating(rating: number) {
+    this.ratingChanged.emit(rating);
+  }
 }
--- ng-lib-examples/example-app/ng-material-frontend/src/app/books/books.module.ts
+++ ng-lib-examples/example-app/ng-material-frontend/src/app/books/books.module.ts
@@ -7,6 +7,7 @@ import { SharedModule } from '../shared/shared.module';
 import { BooksRoutes } from './books.routing';
 import { allContainerComponents } from './container';
 import { allPresentationalComponents } from './presentational';
+import { AngularRatingComponent } from '../../../projects/angular-rating/src/public-api';
 
 @NgModule({
   imports: [
@@ -16,6 +17,7 @@ import { allPresentationalComponents } from './presentational';
     ReactiveFormsModule,
     FormsModule,
     RouterModule.forChild(BooksRoutes),
+    AngularRatingComponent,
   ],
   declarations: [...allContainerComponents, ...allPresentationalComponents],
 })
