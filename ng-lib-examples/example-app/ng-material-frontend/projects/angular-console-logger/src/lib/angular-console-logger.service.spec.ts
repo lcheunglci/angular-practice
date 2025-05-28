92bf9aa4d96cfc9bdf191752cdd3a2b7a62a2a7a
Lawrence LCI <lcheunglci@gmail.com>
2025-05-27 21:13:20 -0700

    Add the test all tool, and add more tests to the logger

--- ng-lib-examples/example-app/ng-material-frontend/projects/angular-console-logger/src/lib/angular-console-logger.service.spec.ts
+++ ng-lib-examples/example-app/ng-material-frontend/projects/angular-console-logger/src/lib/angular-console-logger.service.spec.ts
@@ -20,4 +20,43 @@ describe('AngularConsoleLoggerService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('warn should be called when production is false', () => {
+    const myService = new AngularConsoleLoggerService({
+      appPrefix: 'anything',
+      isProduction: false,
+    });
+
+    const spy = spyOn(console, 'warn');
+
+    myService.warn('this is a test');
+
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it('warn should not be called when production is true', () => {
+    const myService = new AngularConsoleLoggerService({
+      appPrefix: 'anything',
+      isProduction: true,
+    });
+
+    const spy = spyOn(console, 'warn');
+
+    myService.warn('this is a test');
+
+    expect(spy).not.toHaveBeenCalled();
+  });
+
+  it('logged message should have defined prefix', () => {
+    const myService = new AngularConsoleLoggerService({
+      appPrefix: 'anything',
+      isProduction: false,
+    });
+
+    const spy = spyOn(console, 'log');
+
+    myService.warn('this is a test');
+
+    expect(spy).toHaveBeenCalledWith('anything: this is a test');
+  });
 });
