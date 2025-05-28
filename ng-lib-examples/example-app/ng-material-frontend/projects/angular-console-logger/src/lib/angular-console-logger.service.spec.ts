ace46d6e9f601344b6c45e8cf3a3e308dd7f61f2
Lawrence LCI <lcheunglci@gmail.com>
2025-05-27 21:03:19 -0700

    Fix console logger lib test

--- ng-lib-examples/example-app/ng-material-frontend/projects/angular-console-logger/src/lib/angular-console-logger.service.spec.ts
+++ ng-lib-examples/example-app/ng-material-frontend/projects/angular-console-logger/src/lib/angular-console-logger.service.spec.ts
@@ -1,12 +1,19 @@
 import { TestBed } from '@angular/core/testing';
 
 import { AngularConsoleLoggerService } from './angular-console-logger.service';
+import { LoggerConfig } from './logger.config';
 
 describe('AngularConsoleLoggerService', () => {
+  const loggerConfig: LoggerConfig = {
+    appPrefix: 'anything',
+    isProduction: false,
+  };
   let service: AngularConsoleLoggerService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [{ provide: LoggerConfig, useValue: loggerConfig }],
+    });
     service = TestBed.inject(AngularConsoleLoggerService);
   });
 
