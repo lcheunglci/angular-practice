621913ee0967cd24abdb4ef7379fb61827369200
Lawrence LCI <lcheunglci@gmail.com>
2025-06-08 22:59:04 -0700

    Add signal to the quantity for the product selection page

--- ng-signal-example/src/app/products/product-selection/product-selection.ts
+++ ng-signal-example/src/app/products/product-selection/product-selection.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-product-selection',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './product-selection.html',
-  styleUrl: './product-selection.css'
+  styleUrl: './product-selection.css',
 })
 export class ProductSelection {
   pageTitle = 'Product Selection';
 
+  quantity = signal(1);
 }
