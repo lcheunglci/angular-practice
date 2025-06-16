ecb67611c71bf86e56734bf4dbf299e31616127e
Lawrence LCI <lcheunglci@gmail.com>
2025-06-15 18:07:11 -0700

    Avoid making the review request when the selected product is not selected - Switch the request to use the options object

--- ng-signal-example/src/app/reviews/review.service.ts
+++ ng-signal-example/src/app/reviews/review.service.ts
@@ -1,4 +1,4 @@
-import { inject, Injectable } from '@angular/core';
+import { effect, inject, Injectable } from '@angular/core';
 import { ProductService } from '../products/product.service';
 import { httpResource } from '@angular/common/http';
 import { Review } from './review';
@@ -11,11 +11,30 @@ export class ReviewService {
 
   private productService = inject(ProductService);
 
-  reviewsResource = httpResource<Review[]>(
-    () =>
-      `${this.reviewsUrl}?productId=^${
-        this.productService.selectedProduct()?.id
-      }$`,
-    { defaultValue: [] }
+  // reviewsResource = httpResource<Review[]>(
+  //   () => {
+  //     const p = this.productService.selectedProduct();
+  //     if (p) {
+  //       return `${this.reviewsUrl}?productId=^${p.id}$`;
+  //     } else {
+  //       return undefined;
+  //     }
+  //   },
+  //   { defaultValue: [] }
+  // );
+
+  reviewsResource = httpResource<Review[]>(() => ({
+    url: this.reviewsUrl,
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+    },
+    params: {
+      productId: `^${this.productService.selectedProduct()?.id ?? 0}$`,
+    },
+  }));
+
+  eff = effect(() =>
+    console.log('loading reviews', this.reviewsResource.isLoading())
   );
 }
