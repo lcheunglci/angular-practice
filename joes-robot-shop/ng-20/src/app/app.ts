1a0ece27d8491621ee265a691ad156c1f34f167c
Lawrence LCI <lcheunglci@gmail.com>
2025-06-17 23:35:48 -0700

    Update the app component to show the catalog

--- joes-robot-shop/ng-20/src/app/app.ts
+++ joes-robot-shop/ng-20/src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CatalogComponent } from './catalog/catalog.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CatalogComponent],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected title = 'joes-robot-shop';
