5c420dcb77e5151067ebe43e70d771e6301ca8ac
Lawrence LCI <lcheunglci@gmail.com>
2025-06-25 21:42:21 -0700

    Add a cart and cart-item component

---
+++ joes-robot-shop/ng-20/src/app/cart-item/cart-item.component.ts
@@ -0,0 +1,23 @@
+import { Component, input, signal } from '@angular/core';
+import { IProduct } from '../product.model';
+import { CurrencyPipe, NgClass } from '@angular/common';
+
+@Component({
+  selector: 'bot-cart-item',
+  imports: [CurrencyPipe, NgClass],
+  templateUrl: './cart-item.component.html',
+  styleUrl: './cart-item.component.css',
+})
+export class CartItemComponent {
+  product = input.required<IProduct>();
+
+  getImage(product: IProduct) {
+    return '/images/robot-parts/' + product.imageName;
+  }
+
+  removeFromCart() {}
+
+  getPriceClasses() {
+    return { strikethrough: this.product().discount > 0 };
+  }
+}
