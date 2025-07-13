85225bc4592f7526184f3013e142d71ba897a434
Lawrence LCI <lcheunglci@gmail.com>
2025-07-12 23:16:54 -0700

    Add test for triggering event on element

--- ng-unit-test/src/app/heroes/heroes.component.deep.spec.ts
+++ ng-unit-test/src/app/heroes/heroes.component.deep.spec.ts
@@ -47,4 +47,20 @@ describe('HeroesComponent (deep tests)', () => {
         HEROES[i]
       );
   });
+
+  it(`should call heroService.deleteHero when the Hero Component's delete button is clicked`, () => {
+    spyOn(fixture.componentInstance, 'delete');
+    mockHeroService.getHeroes.and.returnValue(of(HEROES));
+
+    fixture.detectChanges();
+
+    const heroComponents = fixture.debugElement.queryAll(
+      By.directive(HeroComponent)
+    );
+    heroComponents[0]
+      .query(By.css('button'))
+      .triggerEventHandler('click', { stopPropagation: () => {} });
+
+    expect(fixture.componentInstance.delete).toHaveBeenCalledWith(HEROES[0]);
+  });
 });
