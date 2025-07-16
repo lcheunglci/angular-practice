478cce3621cf5f6213e2c75af45a01275c051dad
Lawrence LCI <lcheunglci@gmail.com>
2025-07-15 20:10:23 -0700

    Add test with the mocked activate route with the ngModel

--- ng-unit-test/src/app/hero-detail/hero-detail.component.spec.ts
+++ ng-unit-test/src/app/hero-detail/hero-detail.component.spec.ts
@@ -2,10 +2,12 @@ import { ActivatedRoute } from '@angular/router';
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroService } from '../hero.service';
 import { Location } from '@angular/common';
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
+import { FormsModule } from '@angular/forms';
 
 describe('HeroDetailComponent', () => {
-  let fixture;
+  let fixture: ComponentFixture<HeroDetailComponent>;
   let mockHeroService;
   let mockLocation;
   let mockActivatedRoute;
@@ -23,6 +25,7 @@ describe('HeroDetailComponent', () => {
     mockHeroService = jasmine.createSpyObj(['getHero', 'updateHero']);
     mockLocation = jasmine.createSpyObj(['back']);
     TestBed.configureTestingModule({
+      imports: [FormsModule],
       declarations: [HeroDetailComponent],
       providers: [
         { provide: ActivatedRoute, useValue: mockActivatedRoute },
@@ -32,5 +35,16 @@ describe('HeroDetailComponent', () => {
     });
 
     fixture = TestBed.createComponent(HeroDetailComponent);
+    mockHeroService.getHero.and.returnValue(
+      of({ id: 3, name: 'SuperDude', strength: 100 })
+    );
+  });
+
+  it('should render hero name in a h2 tag', () => {
+    fixture.detectChanges();
+
+    expect(fixture.nativeElement.querySelector('h2').textContent).toContain(
+      'SUPERDUDE'
+    );
   });
 });
