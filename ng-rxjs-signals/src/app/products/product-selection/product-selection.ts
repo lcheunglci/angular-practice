9bf493df96a7a154abf4264ec5df9a72da8a831c
Lawrence LCI <lcheunglci@gmail.com>
2025-07-19 22:33:56 -0700

    Add observable for the help

--- ng-rxjs-signals/src/app/products/product-selection/product-selection.ts
+++ ng-rxjs-signals/src/app/products/product-selection/product-selection.ts
@@ -1,18 +1,23 @@
-import { Component, computed, inject } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CurrencyPipe } from '@angular/common';
 import { ProductService } from '../product.service';
 import { ReviewList } from '../../reviews/review-list/review-list';
+import { fromEvent } from 'rxjs';
 
 @Component({
   selector: 'app-product-selection',
   imports: [FormsModule, CurrencyPipe, ReviewList],
   templateUrl: './product-selection.html',
-  styleUrl: './product-selection.css'
+  styleUrl: './product-selection.css',
 })
 export class ProductSelection {
   pageTitle = 'Product Selection';
   private productService = inject(ProductService);
+
+  showHelp = signal(false);
+  questionMark$ = fromEvent<KeyboardEvent>(document, 'keydown');
+  sub = this.questionMark$.subscribe((e) => this.showHelp.set(true));
 
   // Signals used by the template
   selectedProduct = this.productService.selectedProduct;
@@ -21,6 +26,5 @@ export class ProductSelection {
   products = this.productService.productsResource.value;
   isLoading = this.productService.productsResource.isLoading;
   error = this.productService.productsResource.error;
-  errorMessage = computed(() => this.error() ? this.error()?.message : '');
-
+  errorMessage = computed(() => (this.error() ? this.error()?.message : ''));
 }
