5405787b8afc8559879982bd117a917f15d35c4b
Lawrence LCI <lcheunglci@gmail.com>
2025-07-21 19:02:09 -0700

    Change the httpResource to a rxResource in the product.service

--- ng-rxjs-signals/src/app/products/product.service.ts
+++ ng-rxjs-signals/src/app/products/product.service.ts
@@ -1,17 +1,23 @@
-import { httpResource } from '@angular/common/http';
-import { Injectable, signal } from '@angular/core';
+import { HttpClient, httpResource } from '@angular/common/http';
+import { inject, Injectable, signal } from '@angular/core';
 import { Product } from './product';
+import { rxResource } from '@angular/core/rxjs-interop';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductService {
   private productsUrl = 'api/products';
+  private http = inject(HttpClient);
 
   // Signals to support the template
   selectedProduct = signal<Product | undefined>(undefined);
 
   // Retrieve data into a signal
-  productsResource = httpResource<Product[]>(() => this.productsUrl, { defaultValue: [] });
+  // productsResource = httpResource<Product[]>(() => this.productsUrl, { defaultValue: [] });
 
+  productsResource = rxResource({
+    stream: () => this.http.get<Product[]>(this.productsUrl),
+    defaultValue: [],
+  });
 }
