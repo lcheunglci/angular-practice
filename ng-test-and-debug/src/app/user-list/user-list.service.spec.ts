79373790fdc49cb13c54a20c1d6f2d7705cf35bf
Lawrence LCI <lcheunglci@gmail.com>
2025-07-31 00:00:15 -0700

    Fix the Dep Injection error for the httpclient

--- ng-test-and-debug/src/app/user-list/user-list.service.spec.ts
+++ ng-test-and-debug/src/app/user-list/user-list.service.spec.ts
@@ -1,9 +1,17 @@
+import { TestBed } from '@angular/core/testing';
 import { UserListService } from './user-list.service';
+import { HttpClient } from '@angular/common/http';
 
 describe('User List Service', () => {
     let service: UserListService;
 
     beforeEach(() => {
+        TestBed.configureTestingModule({
+            providers: [
+                {provide: HttpClient, useValue: jasmine.createSpyObj('HttpClient', ['get', 'put'])}
+            ]
+        })
+
         service = new UserListService();
     });
 
