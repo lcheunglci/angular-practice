bf8561a386f9926805504b1b51ab922a42bd7758
Lawrence LCI <lcheunglci@gmail.com>
2025-08-01 23:32:54 -0700

    Add custom test bed

--- ng-test-and-debug/src/app/user-list/user-list.component.spec.ts
+++ ng-test-and-debug/src/app/user-list/user-list.component.spec.ts
@@ -3,23 +3,28 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { UserListComponent } from './user-list.component';
 import { WebStorageService } from '../services/web-storage.service';
 import { LocationIdPipe } from '../pipes/location-id.pipe';
+import { CustomTestBed } from '../custom-test-bed';
 
 describe('UserListComponent', () => {
     let component: UserListComponent;
     let fixture: ComponentFixture<UserListComponent>;
 
     beforeEach(async () => {
-        await TestBed.configureTestingModule({
-            declarations: [ UserListComponent, LocationIdPipe ],
-            providers: [
-                { provide: WebStorageService, useValue: jasmine.createSpyObj('WebStorageService', [ 'getRemote', 'setRemote' ]) }
-            ]
+        // await TestBed.configureTestingModule({
+        //     declarations: [ UserListComponent, LocationIdPipe ],
+        //     providers: [
+        //         { provide: WebStorageService, useValue: jasmine.createSpyObj('WebStorageService', [ 'getRemote', 'setRemote' ]) }
+        //     ]
+        // })
+        await CustomTestBed.configureTestingModule({
+            declarations: [UserListComponent],
         })
             .compileComponents();
     });
 
     beforeEach(() => {
-        fixture = TestBed.createComponent(UserListComponent);
+        // fixture = TestBed.createComponent(UserListComponent);
+        fixture = CustomTestBed.createComponent(UserListComponent);
         component = fixture.componentInstance;
         fixture.detectChanges();
     });
