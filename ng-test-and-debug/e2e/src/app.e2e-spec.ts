16f1a7e05e74f4902d1d27ce4500fed7cf4014ae
Lawrence LCI <lcheunglci@gmail.com>
2025-08-02 23:56:46 -0700

    Use the page object in the e2e test

--- ng-test-and-debug/e2e/src/app.e2e-spec.ts
+++ ng-test-and-debug/e2e/src/app.e2e-spec.ts
@@ -1,39 +1,20 @@
-import { browser, by, element } from 'protractor';
 import { AppPage } from './app.po';
 
 describe('User List App', () => {
 
+  const page = new AppPage();
+
   beforeEach(async () => {
-    browser.get('/dashboard');
-  })
-
-  it('should have a header', async () => {
-    const header = by.css('h2');
-    const text = await element(header).getText();
-
-    expect(text).toBe('Active Users');
-  })
-
-  it('should have 16 users on page load', async () => {
-    const items = by.css('li');
-    const users = element.all(items);
-    expect(await users.count()).toBe(16);
-
+    await page.getDashboard();
   });
 
-  //const page = new AppPage();
+  it('should have a header', async () => {
+    const text = await page.getHeaderText();
+    expect(text).toBe('Active Users');
+  });
 
-  // beforeEach(async () => {
-  //   await page.getDashboard();
-  // });
-
-  // it('should have a header', async () => {
-  //   const text = await page.getHeaderText();
-  //   expect(text).toBe('Active Users');
-  // });
-
-  // it('should have 16 users on page load', async () => {
-  //   const users = page.getListItems();
-  //   expect(await users.count()).toBe(16);
-  // });
+  it('should have 16 users on page load', async () => {
+    const users = page.getListItems();
+    expect(await users.count()).toBe(16);
+  });
 });
