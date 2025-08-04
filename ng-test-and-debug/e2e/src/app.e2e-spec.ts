d62c2a1e120cae46c294bc56172696e19d2f9964
Lawrence LCI <lcheunglci@gmail.com>
2025-08-03 23:50:26 -0700

    Add test for input

--- ng-test-and-debug/e2e/src/app.e2e-spec.ts
+++ ng-test-and-debug/e2e/src/app.e2e-spec.ts
@@ -1,7 +1,7 @@
+import { by, ElementArrayFinder, ElementFinder } from 'protractor';
 import { AppPage } from './app.po';
 
 describe('User List App', () => {
-
   const page = new AppPage();
 
   beforeEach(async () => {
@@ -16,5 +16,27 @@ describe('User List App', () => {
   it('should have 16 users on page load', async () => {
     const users = page.getListItems();
     expect(await users.count()).toBe(16);
+  });
+
+  describe('Filter Input', () => {
+    let input: ElementFinder;
+    let items: ElementArrayFinder;
+
+    beforeEach(async () => {
+      input = page.getInput();
+      items = page.getListItems();
+      await input.sendKeys('davis');
+    });
+
+    it('should filter list on user search', async () => {
+      expect(await items.count()).toBe(2);
+    });
+
+    it('should highlight filter text', async () => {
+      (await items).forEach((item) => {
+        const span = item.element(by.css('span'));
+        expect(span.getAttribute('class')).toBe('highlight-text');
+      });
+    });
   });
 });
