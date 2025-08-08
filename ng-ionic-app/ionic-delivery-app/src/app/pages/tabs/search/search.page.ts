d3f8e34d30ee299e311f8fac304220d989e2db09
Lawrence LCI <lcheunglci@gmail.com>
2025-08-07 23:25:44 -0700

    Update the search with some mockdata

--- ng-ionic-app/ionic-delivery-app/src/app/pages/tabs/search/search.page.ts
+++ ng-ionic-app/ionic-delivery-app/src/app/pages/tabs/search/search.page.ts
@@ -1,3 +1,4 @@
+import { Restaurant } from './../../../models/home.model';
 import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
@@ -6,10 +7,44 @@ import { Component, OnInit, ViewChild } from '@angular/core';
   styleUrls: ['./search.page.scss'],
 })
 export class SearchPage implements OnInit {
+  isLoading: boolean = false;
+  query: string = '';
+
+  allRestaurants = [
+    {
+      cover: 'assets/imgs/1.jpg',
+      name: 'Stayfit',
+      short_name: 'stayfit',
+      cuisines: ['Italian', 'Mexican'],
+      rating: 5,
+      delivery_time: 25,
+      price: 100,
+    },
+    {
+      cover: 'assets/imgs/2.jpg',
+      name: 'Stayfit1',
+      short_name: 'stayfit1',
+      cuisines: ['Italian', 'Mexican'],
+      rating: 5,
+      delivery_time: 25,
+      price: 100,
+    },
+    {
+      cover: 'assets/imgs/3.jpg',
+      name: 'Stayfit2',
+      short_name: 'stayfit2',
+      cuisines: ['Italian', 'Mexican'],
+      rating: 5,
+      delivery_time: 25,
+      price: 100,
+    },
+  ];
+
+  restaurants: any[] = [];
 
   @ViewChild('searchInput') searchInput: any;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
     setTimeout(() => {
@@ -19,6 +54,11 @@ export class SearchPage implements OnInit {
 
   onSearchChange(event: any) {
     console.log(event.detail.value);
+    this.query = <string>event.detail.value.toLowerCase();
+    if (this.query.length > 0) {
+      this.restaurants = this.allRestaurants.filter((element: any) =>
+        element.short_name.includes(this.query)
+      );
+    }
   }
-
 }
