1887bf8c81ba210d1cb12c846f52882bc81eb660
Lawrence LCI <lcheunglci@gmail.com>
2025-08-23 23:45:38 -0700

    Add the back button to the cart page

--- ng-ionic-app/ionic-delivery-app/src/app/pages/tabs/cart/cart.page.ts
+++ ng-ionic-app/ionic-delivery-app/src/app/pages/tabs/cart/cart.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cart',
@@ -6,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cart.page.scss'],
 })
 export class CartPage implements OnInit {
+  urlCheck: any;
+  url: any;
 
-  constructor() { }
+  constructor(private router: Router) {}
 
   ngOnInit() {
+    let url: any = this.router.url.split('/');
+    console.log(url);
+
+    const spliced = url.splice(url.length - 2, 2); // /tabs/cart url.length - 1 -1
+    this.urlCheck = spliced[0];
+    console.log('urlcheck', this.urlCheck);
+    url.push(this.urlCheck);
+    this.url = url;
+    console.log(this.url);
   }
 
+  getPreviousUrl() {
+    return this.url.join('/');
+  }
 }
