59eddaa0731522907912c9fc016b2e7bf62a647a
Lawrence LCI <lcheunglci@gmail.com>
2025-09-27 18:42:25 -0700

    Use async to view the change detection in the notification panel

--- ng-zoneless/src/app/notification-panel/notification-panel.ts
+++ ng-zoneless/src/app/notification-panel/notification-panel.ts
@@ -1,82 +1,86 @@
-import { ChangeDetectionStrategy, Component, DoCheck, inject, signal } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  DoCheck,
+  inject,
+  signal,
+} from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { BehaviorSubject } from 'rxjs';
 import { NotificationItemComponent } from '../notification-item/notification-item';
 import { Notification, NotificationService } from './notifications';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
-    selector: 'app-notification-panel',
-    templateUrl: './notification-panel.html',
-    imports: [
-        NotificationItemComponent, 
-        // AsyncPipe
-    ],
-    changeDetection: ChangeDetectionStrategy.OnPush
+  selector: 'app-notification-panel',
+  templateUrl: './notification-panel.html',
+  imports: [NotificationItemComponent, AsyncPipe],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class NotificationPanelComponent implements DoCheck {
-    // --------------------------------------------- Before
-    // protected notifications$ = new BehaviorSubject<Notification[]>([
-    //     { id: crypto.randomUUID(), message: 'Welcome!', read: false },
-    //     { id: crypto.randomUUID(), message: 'Angular is awesome!', read: false }
-    // ]);
-    
-    // protected notifications = this.notifications$.value;
+  // --------------------------------------------- Before
+  protected notifications$ = new BehaviorSubject<Notification[]>([
+    { id: crypto.randomUUID(), message: 'Welcome!', read: false },
+    { id: crypto.randomUUID(), message: 'Angular is awesome!', read: false },
+  ]);
 
-    // --------------------------------------------- Signals
-    protected notifications = signal<Notification[]>([
-        { id: crypto.randomUUID(), message: 'Welcome!', read: false },
-        { id: crypto.randomUUID(), message: 'Angular is awesome!', read: false }
-    ]);
+  protected notifications = this.notifications$.value;
 
-    // --------------------------------------------- Signal From Observable
-    // private incoming = toSignal(this.notificationService.notifications, { initialValue: null });
-    // protected notifications = linkedSignal({
-    //     source: this.incoming,
-    //     computation: (incoming: Notification | null, previous?: { source: Notification | null; value: Notification[]; }) => {
-    //         if (!previous) { 
-    //             return [
-    //                 { id: crypto.randomUUID(), message: 'Welcome!', read: false },
-    //                 { id: crypto.randomUUID(), message: 'Angular is awesome!', read: false }
-    //             ];
-    //         }
-    //         return incoming ? [...previous.value, incoming] : previous.value;
-    //     }
-    // });
+  // --------------------------------------------- Signals
+  // protected notifications = signal<Notification[]>([
+  //     { id: crypto.randomUUID(), message: 'Welcome!', read: false },
+  //     { id: crypto.randomUUID(), message: 'Angular is awesome!', read: false }
+  // ]);
 
-    // --------------------------------------------- Manual Change Detection
-    // private cdRef = inject(ChangeDetectorRef);
+  // --------------------------------------------- Signal From Observable
+  // private incoming = toSignal(this.notificationService.notifications, { initialValue: null });
+  // protected notifications = linkedSignal({
+  //     source: this.incoming,
+  //     computation: (incoming: Notification | null, previous?: { source: Notification | null; value: Notification[]; }) => {
+  //         if (!previous) {
+  //             return [
+  //                 { id: crypto.randomUUID(), message: 'Welcome!', read: false },
+  //                 { id: crypto.randomUUID(), message: 'Angular is awesome!', read: false }
+  //             ];
+  //         }
+  //         return incoming ? [...previous.value, incoming] : previous.value;
+  //     }
+  // });
 
-    private notificationService = inject(NotificationService);
+  // --------------------------------------------- Manual Change Detection
+  // private cdRef = inject(ChangeDetectorRef);
 
-    constructor() {
-        this.notificationService.notifications
-            .pipe(takeUntilDestroyed())
-            .subscribe(notification => {
-                // --------------------------------------------- Before
-                // this.notifications = [...this.notifications, notification];
-                
-                // --------------------------------------------- Async Pipe
-                // this.notifications$.next(this.notifications);
-                
-                // --------------------------------------------- Manual Change Detection
-                // this.cdRef.markForCheck();
-                // this.cdRef.detectChanges();
+  private notificationService = inject(NotificationService);
 
-                // --------------------------------------------- Signals
-                this.notifications.update(v => [...v, notification]);
-            });
-    }
-
-    ngDoCheck() {
-        console.log('NotificationPanel checked');
-    }
-    
-    markAllRead() {
+  constructor() {
+    this.notificationService.notifications
+      .pipe(takeUntilDestroyed())
+      .subscribe((notification) => {
         // --------------------------------------------- Before
-        // this.notifications = this.notifications.map(n => ({ ...n, read: true }));
-        // this.notifications$.next(this.notifications);
+        // this.notifications = [...this.notifications, notification];
+
+        // --------------------------------------------- Async Pipe
+        this.notifications$.next(this.notifications);
+
+        // --------------------------------------------- Manual Change Detection
+        // this.cdRef.markForCheck();
+        // this.cdRef.detectChanges();
 
         // --------------------------------------------- Signals
-        this.notifications.update(v => v.map(n => ({ ...n, read: true })));
-    }
+        // this.notifications.update(v => [...v, notification]);
+      });
+  }
+
+  ngDoCheck() {
+    console.log('NotificationPanel checked');
+  }
+
+  markAllRead() {
+    // --------------------------------------------- Before
+    this.notifications = this.notifications.map((n) => ({ ...n, read: true }));
+    this.notifications$.next(this.notifications);
+
+    // --------------------------------------------- Signals
+    // this.notifications.update(v => v.map(n => ({ ...n, read: true })));
+  }
 }
