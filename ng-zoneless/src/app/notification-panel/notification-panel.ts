1082c4fad3926c902308cdf11cf45e9b502e8776
Lawrence LCI <lcheunglci@gmail.com>
2025-09-28 20:15:21 -0700

    Switch over to signals in the notification panel

--- ng-zoneless/src/app/notification-panel/notification-panel.ts
+++ ng-zoneless/src/app/notification-panel/notification-panel.ts
@@ -6,31 +6,21 @@ import {
   signal,
 } from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
-import { BehaviorSubject } from 'rxjs';
 import { NotificationItemComponent } from '../notification-item/notification-item';
 import { Notification, NotificationService } from './notifications';
-import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-notification-panel',
   templateUrl: './notification-panel.html',
-  imports: [NotificationItemComponent, AsyncPipe],
+  imports: [NotificationItemComponent],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class NotificationPanelComponent implements DoCheck {
-  // --------------------------------------------- Before
-  protected notifications$ = new BehaviorSubject<Notification[]>([
+  // --------------------------------------------- Signals
+  protected notifications = signal<Notification[]>([
     { id: crypto.randomUUID(), message: 'Welcome!', read: false },
     { id: crypto.randomUUID(), message: 'Angular is awesome!', read: false },
   ]);
-
-  protected notifications = this.notifications$.value;
-
-  // --------------------------------------------- Signals
-  // protected notifications = signal<Notification[]>([
-  //     { id: crypto.randomUUID(), message: 'Welcome!', read: false },
-  //     { id: crypto.randomUUID(), message: 'Angular is awesome!', read: false }
-  // ]);
 
   // --------------------------------------------- Signal From Observable
   // private incoming = toSignal(this.notificationService.notifications, { initialValue: null });
@@ -47,27 +37,14 @@ export class NotificationPanelComponent implements DoCheck {
   //     }
   // });
 
-  // --------------------------------------------- Manual Change Detection
-  // private cdRef = inject(ChangeDetectorRef);
-
   private notificationService = inject(NotificationService);
 
   constructor() {
     this.notificationService.notifications
       .pipe(takeUntilDestroyed())
       .subscribe((notification) => {
-        // --------------------------------------------- Before
-        // this.notifications = [...this.notifications, notification];
-
-        // --------------------------------------------- Async Pipe
-        this.notifications$.next(this.notifications);
-
-        // --------------------------------------------- Manual Change Detection
-        // this.cdRef.markForCheck();
-        // this.cdRef.detectChanges();
-
         // --------------------------------------------- Signals
-        // this.notifications.update(v => [...v, notification]);
+        this.notifications.update((v) => [...v, notification]);
       });
   }
 
@@ -76,11 +53,7 @@ export class NotificationPanelComponent implements DoCheck {
   }
 
   markAllRead() {
-    // --------------------------------------------- Before
-    this.notifications = this.notifications.map((n) => ({ ...n, read: true }));
-    this.notifications$.next(this.notifications);
-
     // --------------------------------------------- Signals
-    // this.notifications.update(v => v.map(n => ({ ...n, read: true })));
+    this.notifications.update((v) => v.map((n) => ({ ...n, read: true })));
   }
 }
