2a0435f5eeb9363d7ae24b17ff573ff7b7344667
Lawrence LCI <lcheunglci@gmail.com>
2025-09-28 20:18:49 -0700

    Switch the observable to reactive signal

--- ng-zoneless/src/app/notification-panel/notification-panel.ts
+++ ng-zoneless/src/app/notification-panel/notification-panel.ts
@@ -3,9 +3,9 @@ import {
   Component,
   DoCheck,
   inject,
-  signal,
+  linkedSignal,
 } from '@angular/core';
-import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { toSignal } from '@angular/core/rxjs-interop';
 import { NotificationItemComponent } from '../notification-item/notification-item';
 import { Notification, NotificationService } from './notifications';
 
@@ -16,37 +16,31 @@ import { Notification, NotificationService } from './notifications';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class NotificationPanelComponent implements DoCheck {
-  // --------------------------------------------- Signals
-  protected notifications = signal<Notification[]>([
-    { id: crypto.randomUUID(), message: 'Welcome!', read: false },
-    { id: crypto.randomUUID(), message: 'Angular is awesome!', read: false },
-  ]);
-
-  // --------------------------------------------- Signal From Observable
-  // private incoming = toSignal(this.notificationService.notifications, { initialValue: null });
-  // protected notifications = linkedSignal({
-  //     source: this.incoming,
-  //     computation: (incoming: Notification | null, previous?: { source: Notification | null; value: Notification[]; }) => {
-  //         if (!previous) {
-  //             return [
-  //                 { id: crypto.randomUUID(), message: 'Welcome!', read: false },
-  //                 { id: crypto.randomUUID(), message: 'Angular is awesome!', read: false }
-  //             ];
-  //         }
-  //         return incoming ? [...previous.value, incoming] : previous.value;
-  //     }
-  // });
-
   private notificationService = inject(NotificationService);
 
-  constructor() {
-    this.notificationService.notifications
-      .pipe(takeUntilDestroyed())
-      .subscribe((notification) => {
-        // --------------------------------------------- Signals
-        this.notifications.update((v) => [...v, notification]);
-      });
-  }
+  // --------------------------------------------- Signal From Observable
+  private incoming = toSignal(this.notificationService.notifications, {
+    initialValue: null,
+  });
+  protected notifications = linkedSignal({
+    source: this.incoming,
+    computation: (
+      incoming: Notification | null,
+      previous?: { source: Notification | null; value: Notification[] }
+    ) => {
+      if (!previous) {
+        return [
+          { id: crypto.randomUUID(), message: 'Welcome!', read: false },
+          {
+            id: crypto.randomUUID(),
+            message: 'Angular is awesome!',
+            read: false,
+          },
+        ];
+      }
+      return incoming ? [...previous.value, incoming] : previous.value;
+    },
+  });
 
   ngDoCheck() {
     console.log('NotificationPanel checked');
