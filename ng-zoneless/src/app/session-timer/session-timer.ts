81e0ec20e4ac0fb6884defa3c64bb8c49b8d5091
Lawrence LCI <lcheunglci@gmail.com>
2025-09-29 22:58:14 -0700

    Update the timer to use signals

--- ng-zoneless/src/app/session-timer/session-timer.ts
+++ ng-zoneless/src/app/session-timer/session-timer.ts
@@ -1,47 +1,57 @@
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component, computed, DestroyRef, inject, signal } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  computed,
+  DestroyRef,
+  inject,
+  signal,
+} from '@angular/core';
 
 @Component({
   selector: 'app-session-timer',
   templateUrl: './session-timer.html',
   styleUrls: ['./session-timer.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SessionTimerComponent {
-    readonly total = 15;
-    readonly warnAt = 10;
-    readonly dangerAt = 5;
+  readonly total = 15;
+  readonly warnAt = 10;
+  readonly dangerAt = 5;
 
-    private readonly destroyRef = inject(DestroyRef);
+  private readonly destroyRef = inject(DestroyRef);
 
-    // 1. / 2.
-    protected secondsRemaining = this.total;
-    protected formattedRemaining = this.formatMMSS(this.secondsRemaining);
+  // 1. / 2.
+  // protected secondsRemaining = this.total;
+  // protected formattedRemaining = this.formatMMSS(this.secondsRemaining);
 
-    // 3. --------------------------------------------- Signals
-    // protected readonly secondsRemaining = signal(this.total);
-    // protected readonly formattedRemaining = computed(() => this.formatMMSS(this.secondsRemaining()));
+  // 3. --------------------------------------------- Signals
+  protected readonly secondsRemaining = signal(this.total);
+  protected readonly formattedRemaining = computed(() =>
+    this.formatMMSS(this.secondsRemaining())
+  );
 
-    // 2. --------------------------------------------- Change Detector Ref
-    private cdRef = inject(ChangeDetectorRef);
+  // 2. --------------------------------------------- Change Detector Ref
+  private cdRef = inject(ChangeDetectorRef);
 
-    // 1. / 2.
-    constructor() {
-        const timerId = setInterval(() => {
-            // 1. / 2.
-            this.secondsRemaining = Math.max(this.secondsRemaining - 1, 0);
-            this.formattedRemaining = this.formatMMSS(this.secondsRemaining);
+  // 1. / 2.
+  constructor() {
+    const timerId = setInterval(() => {
+      // 1. / 2.
+      // this.secondsRemaining = Math.max(this.secondsRemaining - 1, 0);
+      // this.formattedRemaining = this.formatMMSS(this.secondsRemaining);
 
-            // 2. --------------------------------------------- Change Detector Ref
-            this.cdRef.markForCheck();
+      // 2. --------------------------------------------- Change Detector Ref
+      // this.cdRef.markForCheck();
 
-            // 3. --------------------------------------------- Signals
-            // this.secondsRemaining.update(v => Math.max(v - 1, 0));
-        }, 1000);
+      // 3. --------------------------------------------- Signals
+      this.secondsRemaining.update((v) => Math.max(v - 1, 0));
+    }, 1000);
 
-        this.destroyRef.onDestroy(() => clearInterval(timerId));
-    }
+    this.destroyRef.onDestroy(() => clearInterval(timerId));
+  }
 
-    private formatMMSS(totalSeconds: number): string {
-        return new Date(totalSeconds * 1000).toISOString().slice(14, 19);
-    }
+  private formatMMSS(totalSeconds: number): string {
+    return new Date(totalSeconds * 1000).toISOString().slice(14, 19);
+  }
 }
