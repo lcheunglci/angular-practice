922ac990b1807a5640eac7dde8cbf33a0a8f0c6f
Lawrence LCI <lcheunglci@gmail.com>
2025-09-30 21:04:09 -0700

    Add the angular/cdk for the products component and add async pipe to it to allow change detection for resize

--- ng-zoneless/src/app/products/products.ts
+++ ng-zoneless/src/app/products/products.ts
@@ -2,66 +2,70 @@ import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { takeUntilDestroyed, toSignal } from '@angular/core/rxjs-interop';
 import { distinctUntilChanged, map } from 'rxjs';
-import { ProductLarge, PRODUCTS_LARGE, PRODUCTS_SMALL, ProductSmall } from './products.data';
+import {
+  ProductLarge,
+  PRODUCTS_LARGE,
+  PRODUCTS_SMALL,
+  ProductSmall,
+} from './products.data';
+import { AsyncPipe } from '@angular/common';
 
 type Layout = 'handset' | 'web';
 
 @Component({
-    selector: 'app-products',
-    templateUrl: './products.html',
-    styleUrl: './products.scss',
-    changeDetection: ChangeDetectionStrategy.OnPush,
-    // imports: [AsyncPipe] // 2.
+  selector: 'app-products',
+  templateUrl: './products.html',
+  styleUrl: './products.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [AsyncPipe], // 2.
 })
 export class ProductsComponent {
-    protected productsLarge: ProductLarge[] = PRODUCTS_LARGE;
-    protected productsSmall: ProductSmall[] = PRODUCTS_SMALL;
-    
-    // 1. --------------------------------------------- Before
-    // protected layout: Layout = 'web';
+  protected productsLarge: ProductLarge[] = PRODUCTS_LARGE;
+  protected productsSmall: ProductSmall[] = PRODUCTS_SMALL;
 
-    private readonly breakpointObserver = inject(BreakpointObserver);
+  // 1. --------------------------------------------- Before
+  // protected layout: Layout = 'web';
 
-    // 1. --------------------------------------------- Before
-    // constructor() {
-    //     this.breakpointObserver
-    //         .observe([Breakpoints.XSmall])
-    //         .pipe(
-    //             takeUntilDestroyed(),
-    //             distinctUntilChanged()
-    //         ).subscribe(state => {
-    //             if (state.breakpoints[Breakpoints.XSmall]) {
-    //               this.layout = 'handset';
-    //             }
-    //             this.layout = 'web';
-    //         });
-    // }
+  private readonly breakpointObserver = inject(BreakpointObserver);
 
-    // 2. --------------------------------------------- AsyncPipe
-    // protected layout = this.breakpointObserver
-    //     .observe([Breakpoints.XSmall])
-    //     .pipe(
-    //         map(state => {
-    //             if (state.matches) {
-    //                 return 'handset';
-    //             }
-    //             return 'web';
-    //         }),
-    //         distinctUntilChanged()
-    // );
+  // 1. --------------------------------------------- Before
+  // constructor() {
+  //     this.breakpointObserver
+  //         .observe([Breakpoints.XSmall])
+  //         .pipe(
+  //             takeUntilDestroyed(),
+  //             distinctUntilChanged()
+  //         ).subscribe(state => {
+  //             if (state.breakpoints[Breakpoints.XSmall]) {
+  //               this.layout = 'handset';
+  //             }
+  //             this.layout = 'web';
+  //         });
+  // }
 
-    // 3. --------------------------------------------- Signals
-    protected layoutObserver = this.breakpointObserver
-        .observe([Breakpoints.XSmall])
-        .pipe(
-            map(state => {
-                if (state.matches) {
-                    return 'handset';
-                }
-                return 'web';
-            }),
-            distinctUntilChanged()
-    );
+  // 2. --------------------------------------------- AsyncPipe
+  protected layout = this.breakpointObserver.observe([Breakpoints.XSmall]).pipe(
+    map((state) => {
+      if (state.matches) {
+        return 'handset';
+      }
+      return 'web';
+    }),
+    distinctUntilChanged()
+  );
 
-    readonly layout = toSignal(this.layoutObserver, { initialValue: 'web' });
+  // 3. --------------------------------------------- Signals
+  // protected layoutObserver = this.breakpointObserver
+  //     .observe([Breakpoints.XSmall])
+  //     .pipe(
+  //         map(state => {
+  //             if (state.matches) {
+  //                 return 'handset';
+  //             }
+  //             return 'web';
+  //         }),
+  //         distinctUntilChanged()
+  // );
+
+  // readonly layout = toSignal(this.layoutObserver, { initialValue: 'web' });
 }
