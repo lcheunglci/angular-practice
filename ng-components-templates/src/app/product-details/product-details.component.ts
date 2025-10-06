0c656c1e1ed6b0cdf0569c525d187bce3edaa3b8
Lawrence LCI <lcheunglci@gmail.com>
2025-10-05 22:32:43 -0700

    Use the i18nPlural pipe for cleaning up the conditional available inventory pipe

--- ng-components-templates/src/app/product-details/product-details.component.ts
+++ ng-components-templates/src/app/product-details/product-details.component.ts
@@ -1,19 +1,28 @@
 import { Component, signal, input } from '@angular/core';
 import { IProduct } from '../product.model';
-import { CurrencyPipe, NgClass } from '@angular/common';
+import { CommonModule } from '@angular/common';
 import { CartService } from '../cart.service';
 
 @Component({
   selector: 'bot-product-details',
-  imports: [CurrencyPipe, NgClass],
+  imports: [CommonModule],
   templateUrl: './product-details.component.html',
-  styleUrl: './product-details.component.css'
+  styleUrl: './product-details.component.css',
 })
 export class ProductDetailsComponent {
   product = input<any>();
   availableInventory = signal(5);
 
-  constructor(private cartService: CartService) { }
+  inventoryMap = {
+    '=0': 'Out of Stock',
+    '=1': 'Only one left!',
+    '=2': 'Few left!',
+    '=3': 'Few left!',
+    '=4': 'Few left!',
+    other: 'Get yours today!',
+  };
+
+  constructor(private cartService: CartService) {}
 
   getImageUrl(product: IProduct) {
     return '/images/robot-parts/' + product.imageName;
@@ -25,6 +34,6 @@ export class ProductDetailsComponent {
   }
 
   getPriceClasses() {
-    return { strikethrough: this.product().discount > 0 }
+    return { strikethrough: this.product().discount > 0 };
   }
 }
