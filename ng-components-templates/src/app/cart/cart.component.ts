adbfc6fead55d1c802cdb718c87384e5d8f698c0
Lawrence LCI <lcheunglci@gmail.com>
2025-10-06 23:00:44 -0700

    Refactor the cart item and reuse the product detail for the remove button

--- ng-components-templates/src/app/cart/cart.component.ts
+++ ng-components-templates/src/app/cart/cart.component.ts
@@ -1,13 +1,14 @@
 import { Component, Signal } from '@angular/core';
-import { CartItemComponent } from "../cart-item/cart-item.component";
+import { CartItemComponent } from '../cart-item/cart-item.component';
 import { IProduct } from '../product.model';
 import { CartService } from '../cart.service';
+import { ProductDetailsComponent } from '../product-details/product-details.component';
 
 @Component({
   selector: 'bot-cart',
-  imports: [CartItemComponent],
+  imports: [CartItemComponent, ProductDetailsComponent],
   templateUrl: './cart.component.html',
-  styleUrl: './cart.component.css'
+  styleUrl: './cart.component.css',
 })
 export class CartComponent {
   cartItems: Signal<IProduct[]>;
@@ -16,4 +17,7 @@ export class CartComponent {
     this.cartItems = this.cartService.cart;
   }
 
+  removeFromCart(product: IProduct) {
+    this.cartService.removeFromCart(product);
+  }
 }
