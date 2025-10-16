7ba43c0d459fc889f7f492a9428bbd9aef15f8a6
Lawrence LCI <lcheunglci@gmail.com>
2025-10-15 23:01:49 -0700

    Add custom preload strategy

--- ng-scalable-arch/src/app/app-routing.module.ts
+++ ng-scalable-arch/src/app/app-routing.module.ts
@@ -1,17 +1,29 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { PreloadingStrategy, Route, RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home';
+import { Observable, of } from 'rxjs';
+
+export class CustomPreloadingStrategy implements PreloadingStrategy {
+  preload(route: Route, load: () => Observable<any>): Observable<any> {
+    if (route.data && route.data['preload']) {
+      return load(); // Preload if 'preload' data property is true
+    }
+    return of(null); // Do not preload
+  }
+
+
+}
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'products', loadComponent: () => import('./product/product-list/product-list').then(m => m.ProductListComponent) },
   { path: 'products/:id', loadComponent: () => import('./product/product-detail/product-detail').then(m => m.ProductDetailComponent) },
   { path: 'user', loadChildren: () => import('@shared/user').then(m => m.UserModule) },
-  { path: 'cart', loadChildren: () => import('@scale/cart').then(m => m.CartModule) },
+  { path: 'cart', loadChildren: () => import('@scale/cart').then(m => m.CartModule), data: { preload: true } },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { preloadingStrategy: CustomPreloadingStrategy})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
