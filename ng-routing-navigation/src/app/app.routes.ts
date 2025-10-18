d675d402c0303bfd5e54f85efe92f878d9efa794
Lawrence LCI <lcheunglci@gmail.com>
2025-10-17 23:57:16 -0700

    Add routing to the app

--- ng-routing-navigation/src/app/app.routes.ts
+++ ng-routing-navigation/src/app/app.routes.ts
@@ -1,11 +1,46 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AllProductsComponent } from './products/all-products/all-products.component';
+import { DetailCardComponent } from './products/product-detail/detail-card/detail-card.component';
+import { AboutComponent } from './about/about.component';
+import { CartComponent } from './cart/cart.component';
+import { ContactComponent } from './contact/contact.component';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
   // home
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
   // products
+  {
+    path: 'products',
+    component: AllProductsComponent,
+  },
   // detail
+  {
+    path: 'detail',
+    component: DetailCardComponent,
+  },
   // login
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
   // contact
+  {
+    path: 'contact',
+    component: ContactComponent,
+  },
   // cart
+  {
+    path: 'cart',
+    component: CartComponent,
+  },
   // about
+  {
+    path: 'about',
+    component: AboutComponent,
+  },
 ];
